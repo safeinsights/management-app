01f37fdb45f171db904ebbfc6125ae6abbaa68c9
Nathan Stitt <nathan@stitt.org>
2024-10-02 20:37:08 -0500

    finalize db access; add study table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 import { footerStyles, mainStyles, pageStyles } from './page.css'
 import { db } from '@/database'
+import { unstable_noStore as noStore } from 'next/cache'
 
 async function getDB() {
     try {
-        return await db.selectFrom('information_schema.tables').selectAll().execute()
+        return await db.selectFrom('study').selectAll().execute()
     } catch (error) {
         console.warn(error)
-        return 'Error getting users'
+        return 'Error getting studies'
     }
 }
 
 export default async function Home() {
+    noStore()
+
     const recs = await getDB()
 
     return (
         <div className={pageStyles}>
-            <main className={mainStyles}>Hello World, found {recs.length} records</main>
+            <main className={mainStyles}>Hello World, found {recs.length} studies</main>
             <footer className={footerStyles}>A SafeInsights production</footer>
         </div>
     )
