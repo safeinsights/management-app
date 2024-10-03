58c12b68fe5ec606c179652bcd0791a3eefaf5ae
therealmarv <1050582+therealmarv@users.noreply.github.com>
2024-10-03 21:53:10 +0200

    wip, get app running but sign up does not work fully yet

---
+++ src/components/login-or-signup.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { Tabs, TextInput, PasswordInput, Button, Flex } from '@mantine/core'
+import { SignIn } from './signin'
+import { SignUp } from './signup'
+
+export const LoginOrSignup: React.FC = () => {
+    return (
+        <Flex justify="center" miw="400px">
+            <Tabs defaultValue="login">
+                <Tabs.List>
+                    <Tabs.Tab value="login">Login</Tabs.Tab>
+                    <Tabs.Tab value="signup">Signup</Tabs.Tab>
+                </Tabs.List>
+
+                <Tabs.Panel value="login" pt="xs">
+                    <SignIn />
+                </Tabs.Panel>
+
+                <Tabs.Panel value="signup" pt="xs">
+                    <SignUp />
+                </Tabs.Panel>
+            </Tabs>
+        </Flex>
+    )
+}
