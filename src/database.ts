1b8e0c825ec5dbb36af6fa38874df38d800fff32
Nathan Stitt <nathan@stitt.org>
2024-10-04 20:13:27 -0500

    don't export

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,6 @@ import { Kysely, CamelCasePlugin } from 'kysely'
 import { DB } from './database/types'
 import { dialect } from './database/dialect'
 import { DEV_ENV } from './lib/util'
-export * from './database/types'
 
 export const db = new Kysely<DB>({
     dialect,
