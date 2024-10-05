2df35ab7a8d5e4cd01ef0ae234bda930f4a07b0e
Nathan Stitt <nathan@stitt.org>
2024-10-04 20:45:35 -0500

    fix env to pass on clerk to docker

--- src/app/admin/members/actions.ts
+++ src/app/admin/members/actions.ts
@@ -4,16 +4,7 @@ import { db } from '@/database'
 import { ValidatedMember, NewMember } from './schema'
 
 export const insertMemberAction = async (member: NewMember) => {
-    const results = await db
-        .insertInto('member')
-        .values({
-            email: member.email,
-            name: member.name,
-            publicKey: member.publicKey,
-            identifier: member.identifier,
-        })
-        .returningAll()
-        .execute()
+    const results = await db.insertInto('member').values(member).returningAll().execute()
 
     if (!results.length) {
         throw new Error('Failed to insert member')
@@ -25,9 +16,7 @@ export const insertMemberAction = async (member: NewMember) => {
 export const updateMemberAction = async (prevIdentifier: string, member: ValidatedMember) => {
     const results = db
         .updateTable('member')
-        .set({
-            name: member.name,
-        })
+        .set(member)
         .where('identifier', '=', prevIdentifier)
         .returningAll('member')
         .execute()
