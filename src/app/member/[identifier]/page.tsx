3d6047a9368249f3b93004d1cc8bc8451e263ef9
Nathan Stitt <nathan@stitt.org>
2024-10-07 12:16:45 -0500

    check that members exist before render
    part of OTTER-28

--- src/app/member/[identifier]/page.tsx
+++ src/app/member/[identifier]/page.tsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import { Form } from './form'
+import { db } from '@/database'
+import { Alert, Flex, Text } from '@mantine/core'
+import { IconError404 } from '@tabler/icons-react'
 
-export default async function MemberHome() {
+export default async function MemberHome({ params }: { params: { identifier: string } }) {
+    const member = await db
+        .selectFrom('member')
+        .select('name')
+        .where('identifier', '=', params.identifier)
+        .executeTakeFirst()
+
+    if (!member) {
+        return (
+            <Alert w="400" m="auto" variant="filled" color="red" icon={<IconError404 />} title="Member not found">
+                Member with identifier {params.identifier} not found
+            </Alert>
+        )
+    }
+
     return (
-        <div>
-            <p>Hmm, a new researcher, you are.</p>
-            <p>Curious, your mind is, yes. Study the ways of knowledge, you seek.</p>
-            <p>Patience, you must have, for many bugs encounter, you will.</p>
-            <p>Questions, ask many, for in the asking, answers revealed, they are.</p>
+        <Flex justify="center">
+            <Flex direction="column">
+                <h1>Welecome to {member.name} data enclave</h1>
 
-            <Form />
-        </div>
+                <Text mt="lg">Hmm, a new researcher, you are.</Text>
+                <Text>Curious, your mind is, yes. Study the ways of knowledge, you seek.</Text>
+                <Text>Patience, you must have, for many bugs encounter, you will.</Text>
+                <Text>Questions, ask many, for in the asking, answers revealed, they are.</Text>
+
+                <Form />
+            </Flex>
+        </Flex>
     )
 }
