9a496cbee90ab416c76a81fbf69cf9cb9b8098a4
Nathan Stitt <nathan@stitt.org>
2024-10-07 19:02:46 -0500

    add auth, build & push instructions

--- src/app/member/[memberIdentifier]/actions.ts
+++ src/app/member/[memberIdentifier]/actions.ts
@@ -1,33 +1,36 @@
 'use server'
 
-import { slugify, DEV_ENV } from '@/lib/util'
-import { ECR } from '@/lib/ecr'
+import { DEV_ENV } from '@/lib/util'
+import { ECR, generateRepositoryPath } from '@/server/ecr'
 import { FormValues, schema } from './schema'
 import { db } from '@/database'
 import { uuidToB64 } from '@/server/uuid'
 
-export const onSubmitAction = async (memberId: string, data: FormValues) => {
-    schema.parse(data) // throws when malformed
+export const onSubmitAction = async (memberId: string, study: FormValues) => {
+    schema.parse(study) // throws when malformed
 
     const member = await db
         .selectFrom('member')
         .select('identifier')
         .where('id', '=', memberId)
         .executeTakeFirstOrThrow()
-    const slug = slugify(data.title)
 
-    let repoUrl = 'not-created-in-dev-mode'
-    if (!DEV_ENV) {
+    const repoPath = generateRepositoryPath(member.identifier, study.title)
+
+    let repoUrl = ''
+    if (DEV_ENV) {
+        repoUrl = '905418271997.dkr.ecr.us-east-1.amazonaws.com/${repoPath}:latest'
+    } else {
         const ecr = new ECR()
-        repoUrl = await ecr.createAnalysisRepository(`${member.identifier}/${slug}`, {
-            title: data.title,
+        repoUrl = await ecr.createAnalysisRepository(repoPath, {
+            title: study.title,
         })
     }
 
     const response = await db
         .insertInto('study')
         .values({
-            title: data.title,
+            title: study.title,
             memberId,
             researcherId: '00000000-0000-0000-0000-000000000000', // FIXME: get researcherId from clerk session
             containerLocation: repoUrl,
