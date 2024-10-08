7cb0adc3a525592162004b7246468cf35405188e
therealmarv <1050582+therealmarv@users.noreply.github.com>
2024-10-08 21:45:50 +0300

    add member sign out etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs"
 import { LoginOrSignup } from "@/components/login-or-signup"
 import { footerStyles, mainStyles, pageStyles } from './page.css'
+import { NavAuthMenu } from "@/components/nav-auth-menu"
 
 export default function Home() {
     return (
@@ -9,7 +10,10 @@ export default function Home() {
                 <LoginOrSignup />
             </SignedOut>
             <SignedIn>
-                <main className={mainStyles}>Hello World</main>
+                <main className={mainStyles}>
+                    <p>Hello World</p>
+                    <NavAuthMenu />
+                </main>
                 <footer className={footerStyles}>A SafeInsights production</footer>
             </SignedIn>
         </div>
