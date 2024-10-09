f289d490999ea2f731a4be3d87ce3d042dbe88d1
Nathan Stitt <nathan@stitt.org>
2024-10-09 13:15:24 -0500

    flesh out create study spec

--- tests/researcher-create-study.spec.tsx
+++ tests/researcher-create-study.spec.tsx
@@ -1,11 +1,16 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('app', () => {
-    test('homepage', async ({ page }) => {
-        // await setupClerkTestingToken({ page });
-
+    test('researcher creates a study', async ({ page }) => {
         await page.goto('/')
         await expect(page).toHaveTitle(/SafeInsights/)
-        await expect(page.getByText('Hello World')).toBeVisible()
+
+        await page.getByRole('button', { name: /study proposal/ }).click()
+
+        // form has an aria-label which playwright will use over the visible label
+        await page.getByLabel(/study name/i).fill('Test Proposal')
+        await page.getByRole('button', { name: /begin/i }).click()
+
+        await expect(page.getByText('Approved')).toBeVisible()
     })
 })
