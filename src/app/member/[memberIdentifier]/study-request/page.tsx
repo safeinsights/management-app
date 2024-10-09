70758910ea9c28ea7dacfa33bbf73d15ec1d3362
Nathan Stitt <nathan@stitt.org>
2024-10-09 11:33:28 -0500

    move pre-propsal screen to study-request location

--- src/app/member/[memberIdentifier]/study-request/page.tsx
+++ src/app/member/[memberIdentifier]/study-request/page.tsx
@@ -3,6 +3,7 @@ import { Form } from './form'
 import { db } from '@/database'
 import { Flex, List, ListItem, Text, Title } from '@mantine/core'
 import { AlertNotFound } from '@/components/alerts'
+import { YodaNotice } from './yoda'
 
 export default async function MemberHome({ params }: { params: { memberIdentifier: string } }) {
     const member = await db
@@ -22,10 +23,10 @@ export default async function MemberHome({ params }: { params: { memberIdentifie
 
     return (
         <Flex justify="center">
+            <YodaNotice />
             <Flex direction="column">
                 <Title>{member.name} Study Pre-Proposal Submission</Title>
-
-                <Text>
+                <Text my="lg">
                     {member.name} is accepting research proposals for post-hoc analysis. Simply follow the steps below
                     to complete the process:
                 </Text>
@@ -37,11 +38,6 @@ export default async function MemberHome({ params }: { params: { memberIdentifie
                         Contingent on approval, OpenStax will proceed to make aggregate results available to you
                     </ListItem>
                 </List>
-
-                <Text mt="lg">Hmm, a new researcher, you are.</Text>
-                <Text>Curious, your mind is, yes. Study the ways of knowledge, you seek.</Text>
-                <Text>Patience, you must have, for many bugs encounter, you will.</Text>
-                <Text>Questions, ask many, for in the asking, answers revealed, they are.</Text>
 
                 <Form memberId={member.id} memberIdentifier={member.identifier} />
             </Flex>
