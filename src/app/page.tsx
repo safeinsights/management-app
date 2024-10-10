79f232a960d3d5efe400365c34404526dfb85a13
Nathan Stitt <nathan@stitt.org>
2024-10-10 16:12:45 -0500

    add rest of fields to form test to get it passing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,16 @@ export default async function Home() {
                 <main className={mainStyles}>
                     <Paper bg="#d3d3d3" shadow="none" p={10} mt={30} mb={-30} radius="sm">
                         <Title>Welcome to the SafeInsights management app.</Title>
+<<<<<<< HEAD
                         <Title order={4}>You likely want to visit the OpenStax study proposal page.</Title>
                         <Link href="/member/openstax/study-request" passHref />
                         <Button>Proceed to study proposal</Button>
+=======
+                        <Title order={4}>You likely want to visit the OpenStax study submission page.</Title>
+                        <Link href="/member/openstax/study-request" passHref>
+                            <Button>Proceed to study</Button>
+                        </Link>
+>>>>>>> dd8335c (add rest of fields to form test to get it passing)
                     </Paper>
                 </main>
             </SignedIn>
