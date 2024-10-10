30f7b545a1ee94e57196f6fabf49821697d07057
Nathan Stitt <nathan@stitt.org>
2024-10-10 15:38:19 -0500

    add clerk support to playwright specs

---
+++ tests/helpers.ts
@@ -0,0 +1,16 @@
+import { type Page, test, expect } from '@playwright/test'
+import { clerk, setupClerkTestingToken } from '@clerk/testing/playwright'
+
+type VisitClerkProtectedPageOptions = { url: string; role: 'researcher'; page: Page }
+export const visitClerkProtectedPage = async ({ page, url }: VisitClerkProtectedPageOptions) => {
+    await setupClerkTestingToken({ page })
+    await page.goto(url)
+    await clerk.signIn({
+        page,
+        signInParams: {
+            strategy: 'password',
+            identifier: process.env.E2E_CLERK_RESEARCHER_EMAIL!,
+            password: process.env.E2E_CLERK_RESEARCHER_PASSWORD!,
+        },
+    })
+}
