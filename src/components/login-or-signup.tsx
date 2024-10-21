479c23a1f529f10faa86f8fead1c29677085e146
therealmarv <1050582+therealmarv@users.noreply.github.com>
2024-10-21 16:52:06 +0300

    allow browser back from signup

--- src/components/login-or-signup.tsx
+++ src/components/login-or-signup.tsx
@@ -1,19 +1,43 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Flex } from '@mantine/core'
 import { SignIn } from './signin'
 import { SignUp } from './signup'
+import { useRouter } from 'next/navigation'
 
 export const LoginOrSignup: React.FC = () => {
     const [showSignIn, setShowSignIn] = useState(true)
+    const router = useRouter()
+
+    useEffect(() => {
+        const handlePopState = () => {
+            setShowSignIn(true)
+        }
+
+        window.addEventListener('popstate', handlePopState)
+
+        return () => {
+            window.removeEventListener('popstate', handlePopState)
+        }
+    }, [])
+
+    const handleSwitchToSignUp = () => {
+        setShowSignIn(false)
+        window.history.pushState(null, '', '/signup')
+    }
+
+    const handleSwitchToSignIn = () => {
+        setShowSignIn(true)
+        router.back()
+    }
 
     return (
         <Flex justify="center" miw="400px">
             {showSignIn ? (
-                <SignIn onSwitchToSignUp={() => setShowSignIn(false)} />
+                <SignIn onSwitchToSignUp={handleSwitchToSignUp} />
             ) : (
-                <SignUp onSwitchToSignIn={() => setShowSignIn(true)} />
+                <SignUp onSwitchToSignIn={handleSwitchToSignIn} />
             )}
         </Flex>
     )
