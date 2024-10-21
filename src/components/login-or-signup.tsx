734ebd9455ebc429feb91f8a5667385fc7d04034
therealmarv <1050582+therealmarv@users.noreply.github.com>
2024-10-21 16:49:01 +0300

    fix state for signin and signup

--- src/components/login-or-signup.tsx
+++ src/components/login-or-signup.tsx
@@ -1,26 +1,20 @@
 'use client'
 
-import { Tabs, Flex } from '@mantine/core'
+import { useState } from 'react'
+import { Flex } from '@mantine/core'
 import { SignIn } from './signin'
 import { SignUp } from './signup'
 
 export const LoginOrSignup: React.FC = () => {
+    const [showSignIn, setShowSignIn] = useState(true)
+
     return (
         <Flex justify="center" miw="400px">
-            <Tabs defaultValue="login">
-                <Tabs.List>
-                    <Tabs.Tab value="login">Login</Tabs.Tab>
-                    <Tabs.Tab value="signup">Signup</Tabs.Tab>
-                </Tabs.List>
-
-                <Tabs.Panel value="login" pt="xs">
-                    <SignIn />
-                </Tabs.Panel>
-
-                <Tabs.Panel value="signup" pt="xs">
-                    <SignUp />
-                </Tabs.Panel>
-            </Tabs>
+            {showSignIn ? (
+                <SignIn onSwitchToSignUp={() => setShowSignIn(false)} />
+            ) : (
+                <SignUp onSwitchToSignIn={() => setShowSignIn(true)} />
+            )}
         </Flex>
     )
 }
