245b26432753f8017bff62067977ac75a71bb7d5
therealmarv <1050582+therealmarv@users.noreply.github.com>
2024-10-23 00:56:00 +0300

    do not allow password paths when logged in

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,19 +1,10 @@
-import { SignedIn, SignedOut } from '@clerk/nextjs'
 import { ResetPassword } from '@/components/reset-password'
-import { Title } from '@mantine/core'
 import { pageStyles } from '../page.css'
 
 export default function ResetPasswordPage() {
     return (
         <div className={pageStyles}>
-            <SignedIn>
-                <Title order={2} c="red">
-                    403 - Forbidden: Password reset is only available for signed out users
-                </Title>
-            </SignedIn>
-            <SignedOut>
-                <ResetPassword />
-            </SignedOut>
+            <ResetPassword />
         </div>
     )
 }
