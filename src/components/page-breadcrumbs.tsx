1c348bf119d57d6de90390de6398566fa633d6c7
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2024-11-07 14:22:13 -0600

    formatting

--- src/components/page-breadcrumbs.tsx
+++ src/components/page-breadcrumbs.tsx
@@ -6,18 +6,18 @@ export const PageBreadcrumbs: React.FC<{
 }> = ({ crumbs }) => {
     return (
         <Breadcrumbs mb={40} separator=">" separatorMargin="md">
-            {crumbs.map(([title, href], index) => (
+            {crumbs.map(([title, href], index) =>
                 href ? (
-                    <Anchor component={Link} href={href} key={index} sx={{ textDecoration: 'underline' }}>
+                    <Anchor component={Link} href={href} key={index}>
                         {title}
                     </Anchor>
                 ) : (
                     <Text key={index}>{title}</Text>
-                )))}
+                ),
+            )}
         </Breadcrumbs>
     )
 }
-
 
 export const MemberBreadcrumbs: React.FC<{
     crumbs: {
@@ -27,16 +27,12 @@ export const MemberBreadcrumbs: React.FC<{
         current?: string
     }
 }> = ({ crumbs: { memberIdentifier, studyIdentifier, studyTitle, current } }) => {
-    const crumbs: Array<[string, string?]> = [
-        ['All Studies', `/member/${memberIdentifier}/studies/review`],
-    ]
+    const crumbs: Array<[string, string?]> = [['All Studies', `/member/${memberIdentifier}/studies/review`]]
     if (studyTitle && studyIdentifier) {
         crumbs.push([studyTitle, `/member/${memberIdentifier}/studies/${studyIdentifier}/review`])
     }
     if (current) {
         crumbs.push([current])
     }
-    return (
-        <PageBreadcrumbs crumbs={crumbs} />
-    )
+    return <PageBreadcrumbs crumbs={crumbs} />
 }
