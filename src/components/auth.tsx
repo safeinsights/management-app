6ddae8c0335229f0d27513f6fbf80d3101774a14
Nathan Stitt <nathan@stitt.org>
2024-11-12 16:09:14 -0600

    check orgs and show appropriate links on homepage

---
+++ src/components/auth.tsx
@@ -0,0 +1,17 @@
+import { useAuth as clerkUseAuth } from "@clerk/nextjs";
+
+export const useAuthInfo = () => {
+    const { isLoaded, userId, orgSlug } = clerkUseAuth()
+    const isAdmin = orgSlug == 'safe-insights'
+    const isMember = orgSlug && !isAdmin
+    const isResearcher = !orgSlug && !isAdmin
+    return {
+        isLoaded,
+        userId,
+        orgSlug,
+        isMember,
+        isAdmin,
+        isResearcher,
+        role: isAdmin ? 'admin' : isMember ? 'member' : 'researcher',
+    }
+};
