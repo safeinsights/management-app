deaf5c4283c22fec7745301d9b0fb00bdc6fd7c0
Nathan Stitt <nathan@stitt.org>
2024-11-12 16:17:44 -0600

    document a bit better

--- bin/debug/request.ts
+++ bin/debug/request.ts
@@ -1,24 +1,19 @@
-import { Command } from 'commander';
+import { Command } from 'commander'
 import fs from 'fs'
 import jwt from 'jsonwebtoken'
 import fetch from 'node-fetch'
 
-
 export class DebugRequest {
-
     program = new Command()
     method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET'
-
     body: Record<string, string> | null = null
     response: Record<string, string> | null = null
-
-    //    path: string = ''
 
     constructor(public path = '') {
         this.program
             .requiredOption('-m, --member <member>', 'member identifier')
             .requiredOption('-o, --origin <origin>', 'base URL to send the request to')
-            .requiredOption('-k, --key <path>', 'Path to the private key file');
+            .requiredOption('-k, --key <path>', 'Path to the private key file')
     }
 
     parse() {
@@ -32,7 +27,7 @@ export class DebugRequest {
         const privateKey = fs.readFileSync(key, 'utf8')
         const payload = {
             iss: member,
-            exp: Math.floor(Date.now() / 1000) + (60 * 60), // Token expiration (1 hour)
+            exp: Math.floor(Date.now() / 1000) + 60 * 60, // Token expiration (1 hour)
         }
         const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' })
         return `Bearer ${token}`
@@ -44,20 +39,18 @@ export class DebugRequest {
         const response = await fetch(`${origin}/api/${this.path}`, {
             method: this.method,
             headers: {
-                'Authorization': this.authorization,
+                Authorization: this.authorization,
                 'Content-Type': 'application/json',
             },
             body: this.body ? JSON.stringify(this.body) : undefined,
         })
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}`)
         }
         if (response.headers.get('content-type')?.includes('application/json')) {
-            this.response = await response.json() as Record<string, string>
+            this.response = (await response.json()) as Record<string, string>
             return this.response
         }
         return {}
-
     }
-
 }
