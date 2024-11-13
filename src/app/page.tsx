b9d47b25ffe50895a0b725342d4e30104cae1073
therealmarv <1050582+therealmarv@users.noreply.github.com>
2024-11-13 23:28:36 +0200

    revert changes in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,10 @@
-'use client'
-
-import { SignedIn, SignedOut, useUser } from '@clerk/nextjs'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
 import { SignIn } from '@/components/signin'
-import { useOrganization } from '@clerk/nextjs'
 import { footerStyles, mainStyles, pageStyles } from './page.css'
 import { Title, Flex } from '@mantine/core'
 import { UserNav } from './user-nav'
 
-export default function Home() {
-    const { user } = useUser()
-    const { organization } = useOrganization()
-    
-    const OPENSTAX_ORG_ID = 'org_2ohzjhfpKp4QqubW86FfXzzDm2I'
-    
-    const isOrgMember = organization?.id === OPENSTAX_ORG_ID
-    const isSiMember = isOrgMember && organization?.membership?.role === 'org:si_member'
-    const isAdmin = isOrgMember && organization?.membership?.role === 'org:admin'
-
-    console.log('Current organization:', organization)
-    console.log(`Active in openstax org: ${isOrgMember ? 'yes' : 'no'}`)
-    console.log(`Is si_member: ${isSiMember ? 'yes' : 'no'}`)
+export default async function Home() {
     return (
         <div className={pageStyles}>
             <SignedOut>
