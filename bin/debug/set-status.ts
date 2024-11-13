ac3775a3b40862dd22ada88f7bc400129e593d52
Nathan Stitt <nathan@stitt.org>
2024-11-13 10:14:07 -0600

    rm debug console.log

--- bin/debug/set-status.ts
+++ bin/debug/set-status.ts
@@ -1,11 +1,7 @@
 import { DebugRequest } from './request'
 
 const req = new DebugRequest()
-console.log('cr opts')
-
 req.program.option('-s, --status <status>', 'status to set').option('-r, --runId <runId>', 'runId to set status for')
-
-console.log('set opts')
 req.parse()
 
 const { status, runId } = req.program.opts()
