7598f5cf6e8e6195b9c667c7b49be4e71256e539
Nathan Stitt <nathan@stitt.org>
2024-11-13 11:10:29 -0600

    add status to table

--- src/app/member/[memberIdentifier]/studies/review/styles.css.ts
+++ src/app/member/[memberIdentifier]/studies/review/styles.css.ts
@@ -6,7 +6,7 @@ export const studyRowStyle = style({
     borderBottom: 'none',
     listStyle: 'none',
     display: 'grid',
-    gridTemplateColumns: '1fr 1fr 200px',
+    gridTemplateColumns: '1fr 1fr 80px 200px',
     gap: '1rem',
     selectors: {
         '&:last-child': {
@@ -15,6 +15,16 @@ export const studyRowStyle = style({
     },
 })
 
+export const studyStatusStyle = style({
+    fontSize: '80%',
+    fontFamily: 'monospace',
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: '1rem',
+    borderLeft: '1px solid #ccc', // Right border on the first cell
+})
+
 export const studyTitleStyle = style({
+    paddingRight: '1rem',
     borderRight: '1px solid #ccc', // Right border on the first cell
 })
