57be46e639c6a0b4020e3a8e13e0fe2a36f91038
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2024-11-14 13:37:35 -0600

    formating

--- src/app/researcher/studies/page.tsx
+++ src/app/researcher/studies/page.tsx
@@ -1,67 +1,60 @@
 import { db } from '@/database'
 import { Container, Flex, Button, Paper, Title, Group, Alert, Anchor } from '@mantine/core'
-import { Panel } from './runs-table'
 import Link from '../../../../node_modules/next/link'
-import { AlertNotFound } from '@/components/errors'
 import { studyRowStyle, studyStatusStyle, studyTitleStyle } from './styles.css'
 import { humanizeStatus } from '@/lib/status'
 
 export const dynamic = 'force-dynamic'
 
 export default async function StudyReviewPage({
-        params: { studyIdentifier },
-    }: {
-        params: {
-            studyIdentifier: string
-        }
-    }) {
-
+    params: { studyIdentifier },
+}: {
+    params: {
+        studyIdentifier: string
+    }
+}) {
     // TODO check user permissions
- 
-    const studies = await db
-    .selectFrom('study')
-    .select(['study.id', 'title', 'piName', 'status'])
-    .orderBy('createdAt', 'desc')
-    .execute()
 
+    const studies = await db
+        .selectFrom('study')
+        .select(['study.id', 'title', 'piName', 'status'])
+        .orderBy('createdAt', 'desc')
+        .execute()
 
     return (
-<>
-
-        <Paper m="xl" shadow="xs" p="xl">
-        <Container width="100%">
-                <Group justify="space-between">
-                    <Title>My Research Proposals</Title>
-                    <Flex justify="flex-end">
-                        <Link href="/researcher/study/request/openstax">
-                            <Button mt={30} mb={30}>
-                                Create A New Proposal
-                            </Button>
-                        </Link>
-                    </Flex>
-                </Group>
-                {studies.length === 0 ? (
-                    <Alert color="gray" title="No studies">
-                        There are no studies to view at this time
-                    </Alert>
-                ) : (
-                    <ul>
-                        {studies.map((study) => (
-                            <li key={study.id} className={studyRowStyle}>
-                                <p className={studyTitleStyle}>{study.title}</p>
-                                <p>{study.piName}</p>
-                                <p className={studyStatusStyle}>{humanizeStatus(study.status)}</p>
-                                <Link href='/researcher/study/review'>
-                                    <Anchor>Proceed to review ≫</Anchor>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-
-            </Container>
-        </Paper>
-</>
-
+        <>
+            <Paper m="xl" shadow="xs" p="xl">
+                <Container width="100%">
+                    <Group justify="space-between">
+                        <Title>My Research Proposals</Title>
+                        <Flex justify="flex-end">
+                            <Link href="/researcher/study/request/openstax">
+                                <Button mt={30} mb={30}>
+                                    Create A New Proposal
+                                </Button>
+                            </Link>
+                        </Flex>
+                    </Group>
+                    {studies.length === 0 ? (
+                        <Alert color="gray" title="No studies">
+                            There are no studies to view at this time
+                        </Alert>
+                    ) : (
+                        <ul>
+                            {studies.map((study) => (
+                                <li key={study.id} className={studyRowStyle}>
+                                    <p className={studyTitleStyle}>{study.title}</p>
+                                    <p>{study.piName}</p>
+                                    <p className={studyStatusStyle}>{humanizeStatus(study.status)}</p>
+                                    <Link href="/researcher/study/review">
+                                        <Anchor>Proceed to review ≫</Anchor>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </Container>
+            </Paper>
+        </>
     )
 }
--- src/app/researcher/studies/review/page.tsx
+++ src/app/researcher/studies/review/page.tsx
@@ -7,15 +7,12 @@ import { MemberBreadcrumbs } from '@/components/page-breadcrumbs'
 import { getStudyAction } from './actions'
 
 export default async function StudyReviewPage() {
-     // TODO check user permissions
-
+    // TODO check user permissions
 
     return (
         <Center>
             <Paper w="50%" shadow="xs" p="sm" m="xs">
-                <Text>
-                    Research Proposal and Research Code Panels will go here!
-                </Text>
+                <Text>Research Proposal and Research Code Panels will go here!</Text>
             </Paper>
         </Center>
     )
