8021629bf31caf214ebb720a2070e84bf9fd600a
Nathan Stitt <nathan@stitt.org>
2024-11-14 15:13:02 -0600

    add IaM policy for enclave access when creating ECR

---
+++ src/server/aws-ecr-policy.ts
@@ -0,0 +1,28 @@
+import { type AwsIAMPolicy, AwsEcrActions as Action } from 'aws-iam-policy-types'
+import { ENCLAVE_AWS_ACCOUNT_NUMBERS } from './config'
+
+export const EcrPolicy: AwsIAMPolicy = {
+    Version: '2012-10-17',
+    Statement: [
+        {
+            Condition: {
+                StringEquals: {
+                    'ecr:ResourceTag/Target': 'si:analysis',
+                },
+            },
+            Action: [
+                Action.BatchCheckLayerAvailability,
+                Action.BatchGetImage,
+                Action.DescribeImages,
+                Action.GetAuthorizationToken,
+                Action.ListTagsForResource,
+            ],
+            Principal: {
+                Service: ['ecs-tasks.amazonaws.com'],
+                AWS: ENCLAVE_AWS_ACCOUNT_NUMBERS.map((acct) => `arn:aws:iam::${acct}:root`),
+            },
+            Effect: 'Allow',
+            Sid: 'AllowEnclaveECSTaskToPullImages',
+        },
+    ],
+}
