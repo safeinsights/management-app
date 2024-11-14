30bb17e8de317ed03d39fd52e9234d0453db0378
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2024-11-14 13:15:47 -0600

    researcher dashboard

---
+++ src/app/researcher/studies/review/page.tsx
@@ -0,0 +1,22 @@
+import { Paper, Center, Title, Text, Stack, Group } from '@mantine/core'
+import { b64toUUID } from '@/lib/uuid'
+import { StudyPanel } from './panel'
+import { AlertNotFound } from '@/components/errors'
+import { getMemberFromIdentifier } from '@/server/members'
+import { MemberBreadcrumbs } from '@/components/page-breadcrumbs'
+import { getStudyAction } from './actions'
+
+export default async function StudyReviewPage() {
+     // TODO check user permissions
+
+
+    return (
+        <Center>
+            <Paper w="50%" shadow="xs" p="sm" m="xs">
+                <Text>
+                    Research Proposal and Research Code Panels will go here!
+                </Text>
+            </Paper>
+        </Center>
+    )
+}
