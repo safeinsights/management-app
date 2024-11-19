9b26eedbadd02693b6c0a87f69209cca0a117d38
Philip Schatz <253202+philschatz@users.noreply.github.com>
2024-11-19 12:16:49 -0800

    adds action to container registry policy

--- src/server/aws-ecr-policy.ts
+++ src/server/aws-ecr-policy.ts
@@ -16,6 +16,7 @@ export const EcrPolicy: AwsIAMPolicy = {
                 Action.DescribeImages,
                 Action.GetAuthorizationToken,
                 Action.ListTagsForResource,
+                Action.GetDownloadUrlForLayer,
             ],
             Principal: {
                 Service: ['ecs-tasks.amazonaws.com'],
