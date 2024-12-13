7e34e2082c305459ade0c5aaa805a4b1805993f3
Nathan Stitt <nathan@stitt.org>
2024-12-13 15:27:26 -0600

    move panda generated files to src/styles/generated

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -16,6 +16,9 @@ export default defineConfig({
             // skipFull: true,
             thresholds: { 100: true },
             include: ['src/**/*.{js,jsx,ts,tsx}'],
+            exclude: [
+                'src/styles/**/*.ts',
+            ]
         },
     },
 })
