852b7ba49daf41697a501bd4a7ad9df985a84c95
Chris Bendel <chrissbendel@gmail.com>
2025-02-04 10:12:53 -0500

    Update package and fix typos

--- src/database/snake_case_types.ts
+++ src/database/snake_case_types.ts
@@ -50,6 +50,7 @@ export interface Study {
 }
 
 export interface StudyRun {
+    code_path: string | null
     completed_at: Timestamp | null
     created_at: Generated<Timestamp>
     file_count: number | null
