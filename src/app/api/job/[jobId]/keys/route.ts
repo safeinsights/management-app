d3638715587fc62fda8d72b33dcd4839e1cfcba6
KC Swanson <kendraswan@gmail.com>
2025-03-03 11:03:47 -0500

    wip get keys

--- src/app/api/job/[jobId]/keys/route.ts
+++ src/app/api/job/[jobId]/keys/route.ts
@@ -3,12 +3,14 @@ import { db } from '@/database'
 export const GET = async (req: Request, { params }: { params: Promise<{jobId: string}> }) => {
   const jobId = (await params).jobId
 
-  // TODO: all the joins
+  // TODO: return a public key per *member user*, not per member
   const publicKeys = await db
     .selectFrom('studyJob')
-    .selectAll()
+    .innerJoin('study', 'study.id', 'studyJob.studyId')
+    .innerJoin('member', 'member.id', 'study.memberId')
+    .select(['studyJob.id as studyJobId', 'studyId', 'study.memberId', 'member.public_key as memberPublicKey'])
     .where('studyJob.id', '=', jobId)
     .execute()
 
-  return Response.json( { 'test': publicKeys } )
+  return Response.json( { 'keys': publicKeys } )
 }
