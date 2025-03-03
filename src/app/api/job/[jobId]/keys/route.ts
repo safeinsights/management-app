c0e86ddbb3f1ac2b216fca0a425d1420eefb7e7f
KC Swanson <kendraswan@gmail.com>
2025-03-03 18:28:29 -0500

    endpoint for public keys

---
+++ src/app/api/job/[jobId]/keys/route.ts
@@ -0,0 +1,16 @@
+import { db } from '@/database'
+
+export const GET = async (req: Request, { params }: { params: Promise<{jobId: string}> }) => {
+  const jobId = (await params).jobId
+
+  const publicKeys = await db
+    .selectFrom('studyJob')
+    .where('studyJob.id', '=', jobId)
+    .innerJoin('study', 'study.id', 'studyJob.studyId')
+    .innerJoin('memberUser', 'memberUser.memberId', 'study.memberId')
+    .innerJoin('memberUserPublicKey', 'memberUserPublicKey.userId', 'memberUser.userId')
+    .select(['studyJob.id as jobId', 'memberUserPublicKey.value as publicKey'])
+    .execute()
+
+  return Response.json( { 'keys': publicKeys } )
+}
