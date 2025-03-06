baf63bfe4a5c0fb8879091e59b952a20ea28cb40
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2025-03-06 14:49:11 -0600

    removed unsused components

--- src/app/user-nav.tsx
+++ src/app/user-nav.tsx
@@ -3,23 +3,20 @@
 import { LoadingOverlay } from '@mantine/core'
 import { useAuthInfo } from '@/components/auth'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
 
 export const UserNav = () => {
     const auth = useAuthInfo()
     const router = useRouter()
 
-
-        if (auth.isLoaded) {
-            if (auth.isResearcher) {
-                router.push('/researcher/dashboard') // Redirect to the Researcher dashboard
-            } else if (auth.isMember) {
-                router.push(`/member/${auth.orgSlug}/dashboard`) // Redirect to the Member dashboard
-            } else if (auth.isAdmin) {
-                router.push('/admin/members') // Redirect to the Admin dashboard
-            }
+    if (auth.isLoaded) {
+        if (auth.isResearcher) {
+            router.push('/researcher/dashboard') // Redirect to the Researcher dashboard
+        } else if (auth.isMember) {
+            router.push(`/member/${auth.orgSlug}/dashboard`) // Redirect to the Member dashboard
+        } else if (auth.isAdmin) {
+            router.push('/admin/members') // Redirect to the Admin dashboard
         }
-
+    }
 
     if (!auth.isLoaded) {
         return <LoadingOverlay />
