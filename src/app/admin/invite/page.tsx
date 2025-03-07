ce654ebf65de543dbec1089a7a05f24a5d7bfe31
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-03-07 16:18:30 +0200

    show organisations in dropdown list

--- src/app/admin/invite/page.tsx
+++ src/app/admin/invite/page.tsx
@@ -1,16 +1,26 @@
 import { Title, Container } from '@mantine/core'
 import { pageStyles, mainStyles, footerStyles } from '@/styles/common'
 import InviteForm from '@/components/clerkcustom/invite-form'
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query'
+import { fetchMembersAction } from '@/server/actions/member-actions'
 
 export default async function InvitePage() {
+    const queryClient = new QueryClient()
+    await queryClient.prefetchQuery({
+        queryKey: ['members'],
+        queryFn: fetchMembersAction,
+    })
+
     return (
-        <div className={pageStyles}>
-            <main className={mainStyles}>
-                <Container size="sm">
-                    <Title mb="lg">Invite New Users</Title>
-                    <InviteForm />
-                </Container>
-            </main>
-        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <div className={pageStyles}>
+                <main className={mainStyles}>
+                    <Container size="sm">
+                        <Title mb="lg">Invite New Users</Title>
+                        <InviteForm />
+                    </Container>
+                </main>
+            </div>
+        </HydrationBoundary>
     )
 }
