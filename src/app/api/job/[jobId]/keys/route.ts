a96c38f6b4c41d3336f35caceb0e70762217042d
KC Swanson <kendraswan@gmail.com>
2025-03-12 10:29:51 -0400

    add fingerprint to selected columns

--- src/app/api/job/[jobId]/keys/route.ts
+++ src/app/api/job/[jobId]/keys/route.ts
@@ -9,7 +9,7 @@ export const GET = async (req: Request, { params }: { params: Promise<{jobId: st
     .innerJoin('study', 'study.id', 'studyJob.studyId')
     .innerJoin('memberUser', 'memberUser.memberId', 'study.memberId')
     .innerJoin('memberUserPublicKey', 'memberUserPublicKey.userId', 'memberUser.userId')
-    .select(['studyJob.id as jobId', 'memberUserPublicKey.value as publicKey'])
+    .select(['studyJob.id as jobId', 'memberUserPublicKey.value as publicKey', 'memberUserPublicKey.fingerprint'])
     .execute()
 
   return Response.json( { 'keys': publicKeys } )
