8a0a07b1d5532fb4f52344c2cf2d974b6b0c7803
Chris Bendel <chrissbendel@gmail.com>
2025-03-13 16:08:42 -0400

    Update some things

--- src/app/researcher/study/request/[memberIdentifier]/study-proposal.tsx
+++ src/app/researcher/study/request/[memberIdentifier]/study-proposal.tsx
@@ -1,27 +1,13 @@
 'use client'
-import React, { useState, useRef } from 'react'
-import { useUser } from '@clerk/nextjs'
+import React, { useRef, useState } from 'react'
 import { useForm } from '@mantine/form'
-import {
-    TextInput,
-    Button,
-    Flex,
-    Accordion,
-    Stack,
-    Text,
-    Group,
-    FileInput,
-    Paper,
-    Divider,
-    Anchor,
-} from '@mantine/core'
-import Link from 'next/link'
+import { Anchor, Button, Divider, FileInput, Flex, Group, Paper, Stack, Text, TextInput } from '@mantine/core'
 import { useRouter } from 'next/navigation'
 import { useMutation } from '@tanstack/react-query'
 import { onCreateStudyAction } from './actions'
 import { css } from '@/styles'
-import { zodResolver, FormValues, schema } from './schema'
-import { UploadStudyJobCode } from '@/components/upload-study-job-code'
+import { StudyProposalFormValues, studyProposalSchema, zodResolver } from './studyProposalSchema'
+
 export const customLabel = css({
     fontSize: '18px',
     marginBottom: '10px',
@@ -29,7 +15,6 @@ export const customLabel = css({
 
 export const StudyProposalForm: React.FC<{ memberId: string; memberIdentifier: string }> = ({ memberId }) => {
     const router = useRouter()
-    const { user } = useUser()
     const fileRefs = {
         description: useRef<HTMLButtonElement>(null),
         irbDocument: useRef<HTMLButtonElement>(null),
@@ -47,26 +32,25 @@ export const StudyProposalForm: React.FC<{ memberId: string; memberIdentifier: s
             ...prev,
             [key]: file ? file.name : null,
         }))
-        studyProposalForm.setFieldValue(key as keyof FormValues, file)
+        studyProposalForm.setFieldValue(key as keyof StudyProposalFormValues, file)
     }
 
-    const [activeSection, setActiveSection] = useState<string | null>(null)
-    const studyProposalForm = useForm<FormValues>({
+    const studyProposalForm = useForm<StudyProposalFormValues>({
         mode: 'uncontrolled',
-        validate: zodResolver(schema),
+        validate: zodResolver(studyProposalSchema),
         validateInputOnBlur: true,
         initialValues: {
             title: '',
             piName: '',
-            studyLead: '',
-            description: undefined,
-            irbDocument: undefined,
-            // TOO: Add agreement document
+            // studyLead: '',
+            description: null,
+            irbDocument: null,
+            // TODO: Add agreement document
         },
     })
 
     const { mutate: createStudy, isPending } = useMutation({
-        mutationFn: async (d: FormValues) => await onCreateStudyAction(memberId, d),
+        mutationFn: async (d: StudyProposalFormValues) => await onCreateStudyAction(memberId, d),
         onSettled(result, error) {
             if (error || !result?.studyId) {
                 studyProposalForm.setErrors({
@@ -81,100 +65,99 @@ export const StudyProposalForm: React.FC<{ memberId: string; memberIdentifier: s
 
     return (
         <>
-                <form onSubmit={studyProposalForm.onSubmit((values) => createStudy(values))}>
-                    <Paper p="xl">
-                        <Text>Researcher Study Proposal</Text>
-                        <Divider my="sm" mt="sm" mb="md" />
-                        <Text>
-                            This section is here to help you submit your study proposal. Consider providing as much
-                            detail as possible to ensure the Reviewer has all the information needed to make an informed
-                            decision.
-                        </Text>
-                        <Group mt="xl">
-                            <Stack>
-                                <Group>
-                                    <Text>Study Title</Text>
-                                    <TextInput
-                                        bd="1px solid #000000"
-                                        placeholder="Enter a title (max. 50 characters)"
-                                        required
-                                        name="title"
-                                        key={studyProposalForm.key('title')}
-                                        {...studyProposalForm.getInputProps('title')}
-                                        className={customLabel}
-                                    />
-                                </Group>
-                                <Group>
-                                    <Text>Study Lead</Text>
-                                    <TextInput
-                                        bg="#ddd"
-                                        bd="1px solid #000000"
-                                        disabled
-                                        name="study-lead"
-                                        value="Researcher Name"
-                                        // value={user?.firstName + ' ' + user?.lastName}
-                                        data-testid="study-lead"
-                                    />
-                                </Group>
-                                <Group>
-                                    <Text>Principal Investigator</Text>
-                                    <TextInput
-                                        bd="1px solid #000000"
-                                        name="piName"
-                                        required
-                                        key={studyProposalForm.key('piName')}
-                                        {...studyProposalForm.getInputProps('piName')}
-                                        className={customLabel}
-                                    />
-                                </Group>
-                                <Group>
-                                    <Text>Study Description</Text>
-                                    <FileInput
-                                        bd="1px solid #000000"
-                                        ref={fileRefs.description}
-                                        name="description"
-                                        placeholder="Upload a document describing your study"
-                                        required
-                                        key={studyProposalForm.key('description')}
-                                        {...studyProposalForm.getInputProps('description')}
-                                        onChange={(file) => handleFileChange('description', file)}
-                                        className={customLabel}
-                                        style={{ display: 'none' }}
-                                    />
-                                    <Anchor
-                                        component="button"
-                                        onClick={() => fileRefs.description.current?.click()}
-                                        underline="always"
-                                    >
-                                        Upload
-                                    </Anchor>
-                                    {fileNames.description && <Text> {fileNames.description}</Text>}
-                                </Group>
-                                <Group>
-                                    <Text>IRB Document</Text>
-                                    <FileInput
-                                        bd="1px solid #000000"
-                                        ref={fileRefs.irbDocument}
-                                        name="irbDocument"
-                                        placeholder="Upload IRB approval document"
-                                        required
-                                        key={studyProposalForm.key('irbDocument')}
-                                        {...studyProposalForm.getInputProps('irbDocument')}
-                                        onChange={(file) => handleFileChange('irbDocument', file)}
-                                        className={customLabel}
-                                        style={{ display: 'none' }}
-                                    />
-                                    <Anchor
-                                        component="button"
-                                        onClick={() => fileRefs.irbDocument.current?.click()}
-                                        underline="always"
-                                    >
-                                        Upload
-                                    </Anchor>
-                                    {fileNames.irbDocument && <Text> {fileNames.irbDocument}</Text>}
-                                </Group>
-                                {/* <Text>Agreement Document</Text> TODO: Need Database column for this attribute */}
-                                {/* <FileInput
+            <form onSubmit={studyProposalForm.onSubmit((values) => createStudy(values))}>
+                <Paper p="xl">
+                    <Text>Researcher Study Proposal</Text>
+                    <Divider my="sm" mt="sm" mb="md" />
+                    <Text>
+                        This section is here to help you submit your study proposal. Consider providing as much detail
+                        as possible to ensure the Reviewer has all the information needed to make an informed decision.
+                    </Text>
+                    <Group mt="xl">
+                        <Stack>
+                            <Group>
+                                <Text>Study Title</Text>
+                                <TextInput
+                                    bd="1px solid #000000"
+                                    placeholder="Enter a title (max. 50 characters)"
+                                    required
+                                    name="title"
+                                    key={studyProposalForm.key('title')}
+                                    {...studyProposalForm.getInputProps('title')}
+                                    className={customLabel}
+                                />
+                            </Group>
+                            <Group>
+                                <Text>Study Lead</Text>
+                                <TextInput
+                                    bg="#ddd"
+                                    bd="1px solid #000000"
+                                    disabled
+                                    name="study-lead"
+                                    value="Researcher Name"
+                                    // value={user?.firstName + ' ' + user?.lastName}
+                                    data-testid="study-lead"
+                                />
+                            </Group>
+                            <Group>
+                                <Text>Principal Investigator</Text>
+                                <TextInput
+                                    bd="1px solid #000000"
+                                    name="piName"
+                                    required
+                                    key={studyProposalForm.key('piName')}
+                                    {...studyProposalForm.getInputProps('piName')}
+                                    className={customLabel}
+                                />
+                            </Group>
+                            <Group>
+                                <Text>Study Description</Text>
+                                <FileInput
+                                    bd="1px solid #000000"
+                                    ref={fileRefs.description}
+                                    name="description"
+                                    placeholder="Upload a document describing your study"
+                                    required
+                                    key={studyProposalForm.key('description')}
+                                    {...studyProposalForm.getInputProps('description')}
+                                    onChange={(file) => handleFileChange('description', file)}
+                                    className={customLabel}
+                                    style={{ display: 'none' }}
+                                />
+                                <Anchor
+                                    component="button"
+                                    onClick={() => fileRefs.description.current?.click()}
+                                    underline="always"
+                                >
+                                    Upload
+                                </Anchor>
+                                {fileNames.description && <Text> {fileNames.description}</Text>}
+                            </Group>
+                            <Group>
+                                <Text>IRB Document</Text>
+                                <FileInput
+                                    bd="1px solid #000000"
+                                    ref={fileRefs.irbDocument}
+                                    name="irbDocument"
+                                    placeholder="Upload IRB approval document"
+                                    required
+                                    key={studyProposalForm.key('irbDocument')}
+                                    {...studyProposalForm.getInputProps('irbDocument')}
+                                    onChange={(file) => handleFileChange('irbDocument', file)}
+                                    className={customLabel}
+                                    style={{ display: 'none' }}
+                                />
+                                <Anchor
+                                    component="button"
+                                    onClick={() => fileRefs.irbDocument.current?.click()}
+                                    underline="always"
+                                >
+                                    Upload
+                                </Anchor>
+                                {fileNames.irbDocument && <Text> {fileNames.irbDocument}</Text>}
+                            </Group>
+                            {/* <Text>Agreement Document</Text> TODO: Need Database column for this attribute */}
+                            {/* <FileInput
                                     bd="1px solid #000000"
                                     ref={fileRefs.agreementDocument}
                                     name="agreementDocument"
@@ -193,15 +176,15 @@ export const StudyProposalForm: React.FC<{ memberId: string; memberIdentifier: s
                                 >
                                     Upload
                                 </Anchor>
-                                {fileNames.agreementDocument && ( 
+                                {fileNames.agreementDocument && (
                                     <Text> {fileNames.agreementDocument}</Text>
                                 )} */}
-                            </Stack>
-                            <Stack mt="md">
-                                <Flex direction="column" gap="sm">
-                                    {/* TODO: Need Database column for this attribute */}
-                                    {/* <FileInput
-                                                    
+                        </Stack>
+                        <Stack mt="md">
+                            <Flex direction="column" gap="sm">
+                                {/* TODO: Need Database column for this attribute */}
+                                {/* <FileInput
+
                                                     bd="1px solid #000000"
                                                     name="agreementDocument"
                                                     placeholder="Upload agreement document"
@@ -210,34 +193,34 @@ export const StudyProposalForm: React.FC<{ memberId: string; memberIdentifier: s
                                                     {...form.getInputProps('agreementDocument')}
                                                     className={customLabel}
                                                 /> */}
-                                </Flex>
-                            </Stack>
-                        </Group>
-                    </Paper>
-                    <Group gap="xl" p={2} mt="xl" justify="flex-end">
-                        <Button
-                            fz="lg"
-                            mb="lg"
-                            type="button"
-                            onClick={() => router.push(`/`)}
-                            variant="outline"
-                            color="#616161"
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            fz="lg"
-                            mb="lg"
-                            type="submit"
-                            disabled={!studyProposalForm.isValid}
-                            variant="filled"
-                            color="#616161"
-                            loading={isPending}
-                        >
-                            Next
-                        </Button>
+                            </Flex>
+                        </Stack>
                     </Group>
-                </form>
+                </Paper>
+                <Group gap="xl" p={2} mt="xl" justify="flex-end">
+                    <Button
+                        fz="lg"
+                        mb="lg"
+                        type="button"
+                        onClick={() => router.push(`/`)}
+                        variant="outline"
+                        color="#616161"
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        fz="lg"
+                        mb="lg"
+                        type="submit"
+                        disabled={!studyProposalForm.isValid}
+                        variant="filled"
+                        color="#616161"
+                        loading={isPending}
+                    >
+                        Next
+                    </Button>
+                </Group>
+            </form>
         </>
     )
 }
