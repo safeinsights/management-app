200242187c0a5edb4072c7f2418436b4e7a742ee
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2025-03-13 03:20:19 -0500

    Study details - study proposal panel

--- src/app/researcher/study/[encodedStudyId]/edit/form.tsx
+++ src/app/researcher/study/[encodedStudyId]/edit/form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Checkbox, Textarea, TextInput, Button, Flex, Group, Stack, Text } from '@mantine/core'
+import { Checkbox, Textarea, TextInput, Button, Flex, Group, Stack, Text, Paper, Title } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { onUpdateStudyAction } from './actions'
 import { FormValues, schema, zodResolver } from './schema'
@@ -26,129 +26,104 @@ export const Form: React.FC<{ studyId: string; study: FormValues }> = ({ studyId
         validate: zodResolver(schema),
         validateInputOnBlur: true,
         initialValues: {
-            highlights: true,
-            eventCapture: true,
-            irbDocument: 'IRB Document.pdf',
+            irbDocument: study.irbProtocols,
             ...study,
         },
     })
 
     return (
         <form onSubmit={form.onSubmit((values) => updateStudy(values))}>
-            <Stack>
-                <Text size="xl" fz="30px" ta="left" mb={30}>
-                    OpenStax Study Proposal Step 3)
-                </Text>
-                <Text size="xl" ta="left">
-                    STUDY DETAILS
-                </Text>
+            <Paper p="md" mb="md">
+                <Title>Study Details</Title>
+            </Paper>
+            <Paper p="md">
+                <Stack>
+                    <Text size="xl" ta="left">
+                        Study Proposal
+                    </Text>
 
-                <Flex p={2} gap="md" wrap="wrap">
-                    <Text className={labelStyle}>Study Title</Text>
-                    <TextInput
-                        bg="#ddd"
-                        bd="1px solid #ccc"
-                        disabled
-                        className={inputStyle}
-                        data-testid="study-title"
-                        key={form.key('title')}
-                        {...form.getInputProps('title')}
-                        readOnly
-                    />
-                </Flex>
+                    <Flex p={2} gap="md" wrap="wrap">
+                        <Text className={labelStyle}>Study Title</Text>
+                        <TextInput
+                            bg="#ddd"
+                            bd="1px solid #ccc"
+                            disabled
+                            className={inputStyle}
+                            data-testid="study-title"
+                            key={form.key('title')}
+                            {...form.getInputProps('title')}
+                            readOnly
+                        />
+                    </Flex>
+                    <Flex p={2} gap="md" wrap="wrap">
+                        <Text className={labelStyle}>Study Lead</Text>
+                        <TextInput
+                            bg="#ddd"
+                            bd="1px solid #ccc"
+                            disabled
+                            className={inputStyle}
+                            data-testid="study-title"
+                            key={form.key('study-lead')}
+                            {...form.getInputProps('study-lead')}
+                            readOnly
+                        />
+                    </Flex>
 
-                <Flex p={2} gap="md" wrap="wrap">
-                    <Text className={labelStyle} component="span">
-                        Study Description
-                        <Text component="span" c="red" inherit>
-                            *
+                    <Flex p={2} gap="md">
+                        <Text className={labelStyle} component="span">
+                            Principal Investigator
+                            <Text component="span" c="red" inherit>
+                                *
+                            </Text>
                         </Text>
-                    </Text>
-                    <Textarea
-                        className={inputStyle}
-                        name="description"
-                        label=""
-                        key={form.key('description')}
-                        {...form.getInputProps('description')}
-                    />
-                </Flex>
-
-                <Flex p={2} gap="md">
-                    <Text className={labelStyle} component="span">
-                        Principal Investigator
-                        <Text component="span" c="red" inherit>
-                            *
+                        <TextInput
+                            bg="#ddd"
+                            bd="1px solid #ccc"
+                            className={inputStyle}
+                            name="piName"
+                            disabled
+                            data-testid="piName"
+                            key={form.key('piName')}
+                            {...form.getInputProps('piName')}
+                        />
+                    </Flex>
+                    <Flex p={2} gap="md" wrap="wrap">
+                        <Text className={labelStyle} component="span">
+                            Study Description
+                            <Text component="span" c="red" inherit>
+                                *
+                            </Text>
                         </Text>
-                    </Text>
-                    <TextInput
-                        className={inputStyle}
-                        name="piName"
-                        key={form.key('piName')}
-                        {...form.getInputProps('piName')}
-                    />
-                </Flex>
-                <Group p={2} gap="md">
-                    <Text className={labelStyle}>IRB Approval Documentation</Text>
-                    <TextInput
-                        bg="#ddd"
-                        bd="1px solid #ccc"
-                        disabled
-                        className={inputStyle}
-                        name="irbDocument"
-                        key={form.key('irbDocument')}
-                        {...form.getInputProps('irbDocument')}
-                        readOnly
-                    />
-                    <Text fs="italic" c="dimmed" w="20%">
-                        {'For the pilot, we are skipping the IRB step'}
-                    </Text>
-                </Group>
-            </Stack>
+                        <TextInput
+                            bg="#ddd"
+                            bd="1px solid #ccc"
+                            className={inputStyle}
+                            name="description"
+                            disabled
+                            label=""
+                            key={form.key('description')}
+                            {...form.getInputProps('description')}
+                            readOnly
+                        />
+                    </Flex>
 
-            <Text size="xl" ta="left" mt={50} mb={10}>
-                REQUESTED DATA DETAILS
-            </Text>
-            <Stack align="stretch">
-                <Flex p={2} gap="lg">
-                    <Text className={labelStyle} component="span">
-                        Datasets of Interest
-                        <Text component="span" c="red" inherit>
-                            *
-                        </Text>
-                    </Text>
-                    <Stack>
-                        <Checkbox
-                            name="highlights"
-                            label="Highlights and Notes"
-                            key={form.key('highlights')}
-                            {...form.getInputProps('highlights', { type: 'checkbox' })}
-                        ></Checkbox>
-                        <Checkbox
-                            name="eventCapture"
-                            label="Event Capture"
-                            key={form.key('eventCapture')}
-                            {...form.getInputProps('eventCapture', { type: 'checkbox' })}
-                            error={form.errors['dataSources']}
-                        ></Checkbox>
-                    </Stack>
-                </Flex>
-                <Flex p={2} gap="md" wrap="wrap">
-                    <Text className={labelStyle}>Container URL</Text>
-                    <TextInput
-                        bg="#ddd"
-                        bd="1px solid #ccc"
-                        disabled
-                        className={inputStyle}
-                        name="containerLocation"
-                        {...form.getInputProps('containerLocation')}
-                        data-testid="container-location"
-                        readOnly
-                    />
-                </Flex>
-            </Stack>
-
+                    <Group p={2} gap="md">
+                        <Text className={labelStyle}>IRB Document</Text>
+                        <TextInput
+                            bg="#ddd"
+                            bd="1px solid #ccc"
+                            disabled
+                            className={inputStyle}
+                            name="irbDocument"
+                            key={form.key('irbDocument')}
+                            {...form.getInputProps('irbDocument')}
+                            readOnly
+                        />
+                    </Group>
+                </Stack>
+            </Paper>
             <Group gap="xl" p={2} mt={40} justify="flex-end">
-                <Link href="/researcher/studies" passHref>
+                <Link href="/researcher/dashboard" passHref>
                     <Button disabled={!form.isValid || isPending}>Back to all studies</Button>
                 </Link>
                 <Button disabled={!form.isValid || isPending} type="submit" variant="default">
