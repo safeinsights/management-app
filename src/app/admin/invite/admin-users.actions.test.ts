f521a497cfd7de86a396896e3e84b9fa1ae8d2c4
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-03-26 17:00:19 +0200

    fix tests

--- src/app/admin/invite/admin-users.actions.test.ts
+++ src/app/admin/invite/admin-users.actions.test.ts
@@ -1,5 +1,6 @@
 import { db } from '@/database'
 import { describe, it, expect, vi, beforeEach, Mock } from 'vitest'
+import { CLERK_ADMIN_ORG_SLUG } from '@/server/config'
 import { mockClerkSession } from '@/tests/unit.helpers'
 import { adminInviteUserAction } from './admin-users.actions'
 import { faker } from '@faker-js/faker'
