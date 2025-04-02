9aeb13b10475f9795af849d9236e9100b7acc511
Chris Bendel <chrissbendel@gmail.com>
2025-04-02 15:03:21 -0400

    Dont render signup page on prod

--- src/app/account/signup/page.tsx
+++ src/app/account/signup/page.tsx
@@ -1,8 +1,13 @@
 import { SignUp } from '@/components/signup'
 import { pageStyles } from '@/styles/common'
 import { Container } from '@/styles/generated/jsx'
+import { PROD_ENV } from '@/server/config'
 
 export default function SignUpPage() {
+    // https://openstax.atlassian.net/browse/OTTER-107
+    // Temporarily remove signup page on production
+    if (PROD_ENV) return null
+
     return (
         <div className={pageStyles}>
             <Container>
