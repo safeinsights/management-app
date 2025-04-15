5d903d3095a60a58edcad4c1d18675c2b42f4822
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-04-15 19:18:39 +0300

    hack Personal Account with help of localization to Researcher Account

--- src/components/layout/user-layout.tsx
+++ src/components/layout/user-layout.tsx
@@ -12,7 +12,14 @@ export async function UserLayout({ children }: Props) {
     const key = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || ''
 
     return (
-        <ClerkProvider publishableKey={key}>
+        <ClerkProvider
+            publishableKey={key}
+            localization={{
+                organizationSwitcher: {
+                    personalWorkspace: 'Researcher Account',
+                },
+            }}
+        >
             <AppShell>{children}</AppShell>
         </ClerkProvider>
     )
