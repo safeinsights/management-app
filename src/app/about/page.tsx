06835268ea3a7f61fb2a5b5c791afc3b1797d5e4
Nathan Stitt <nathan@stitt.org>
2025-04-18 16:24:18 -0500

    add link to view sha

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
-import { Paper, Card, Text, Divider } from '@mantine/core'
-
+import { Paper, Card, Text, Divider, Flex } from '@mantine/core'
+import { Eye } from '@phosphor-icons/react/dist/ssr'
 export const dynamic = 'force-dynamic'
 
-const Stat = ({ title, value }: { title: string; value: string }) => (
+const Stat = ({ title, value }: { title: string; value: React.ReactNode }) => (
     <>
         <Text fz="lg" fw={500}>
             {title}:
@@ -17,7 +17,13 @@ export default function AboutPage() {
     return (
         <Paper bg="#d3d3d3" shadow="none" p={10} mt={30} radius="sm" miw={500} maw={800} mx="auto">
             <Card withBorder radius="md" padding="xl" bg="var(--mantine-color-body)">
-                <Stat title="Release SHA" value={process.env.RELEASE_SHA || 'unknown'} />
+                <Stat
+                    title="Release SHA"
+                    value={process.env.RELEASE_SHA ? <Flex gap="md" align={'center'}>
+                                                         {process.env.RELEASE_SHA}
+                                                         <a href={`https://github.com/safeinsights/management-app/commit/${process.env.RELEASE_SHA}`}><Eye /></a>
+                                                     </Flex> : 'not deployed'}
+                />
 
                 <Divider my="md" />
 
