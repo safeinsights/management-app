a5ea6fb127f1fe361d14eba2da0e2610df9702b2
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2025-04-29 12:11:15 -0500

    style approve and reject buttons

--- src/app/reviewer/[memberSlug]/study/[studyIdentifier]/review/study-review-buttons.tsx
+++ src/app/reviewer/[memberSlug]/study/[studyIdentifier]/review/study-review-buttons.tsx
@@ -13,6 +13,7 @@ import {
 
 import { CheckCircle, XCircle } from '@phosphor-icons/react/dist/ssr'
 import dayjs from 'dayjs'
+import { theme } from '@/theme'
 
 export const StudyReviewButtons: FC<{ study: SelectedStudy; memberSlug: string }> = ({ study, memberSlug }) => {
     const router = useRouter()
@@ -41,18 +42,22 @@ export const StudyReviewButtons: FC<{ study: SelectedStudy; memberSlug: string }
 
     if (study.status === 'APPROVED' && study.approvedAt) {
         return (
-            <Group c="#12B886" gap="0">
-                <CheckCircle weight="fill" />
-                <Text>Approved on {dayjs(study.approvedAt).format('MMM DD, YYYY')}</Text>
+            <Group c="#12B886" gap="0.5rem" align="center">
+                <CheckCircle weight="fill" size={24} color={theme.colors.green[9]} />
+                <Text fz="xs" fw="semibold" c="green.9">
+                    Approved on {dayjs(study.approvedAt).format('MMM DD, YYYY')}
+                </Text>
             </Group>
         )
     }
 
     if (study.status === 'REJECTED' && study.rejectedAt) {
         return (
-            <Group c="#FA5252" gap="0">
-                <XCircle weight="fill" />
-                <Text>Rejected on {dayjs(study.rejectedAt).format('MMM DD, YYYY')}</Text>
+            <Group c="#FA5252" gap="0.5rem">
+                <XCircle weight="fill" size={24} color={theme.colors.red[9]} />
+                <Text fz="xs" fw="semibold" c="red.9">
+                    Rejected on {dayjs(study.rejectedAt).format('MMM DD, YYYY')}
+                </Text>
             </Group>
         )
     }
