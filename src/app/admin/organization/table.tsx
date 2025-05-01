5503b4b901ce706fef462ab878c236da7dadd9da
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2025-05-01 07:35:38 -0500

    remove fragments

--- src/app/admin/organization/table.tsx
+++ src/app/admin/organization/table.tsx
@@ -29,64 +29,62 @@ export function MembersAdminTable() {
     }, [data, sortStatus])
 
     return (
-        <>
-            <Stack p="xl">
-                <AdminBreadcrumbs crumbs={{ current: 'Manage team' }}></AdminBreadcrumbs>
-                <Title order={1}>Manage Team</Title>
-                <Paper shadow="xs" p="xl">
-                    <Stack>
-                        <Group justify="space-between">
-                            <Title order={3}>People</Title>
-                            <Flex justify="flex-end">
-                                <Link href="/admin/invite">
-                                    <Button leftSection={<Plus />}>Invite People</Button>
-                                </Link>
-                            </Flex>
-                        </Group>
-                        <Divider c="charcoal.1" />
-                        <DataTable
-                            withColumnBorders
-                            striped
-                            backgroundColor="charcoal.1"
-                            idAccessor="slug"
-                            noRecordsText="No organisations yet, add some using button below"
-                            noRecordsIcon={<Users />}
-                            records={sortedMembers}
-                            sortStatus={sortStatus}
-                            onSortStatusChange={setSortStatus}
-                            columns={[
-                                { accessor: 'name', sortable: true },
-                                {
-                                    accessor: 'role',
-                                    sortable: false,
-                                    title: (
-                                        <Group gap="xs">
-                                            <Text>Role</Text>
-                                            <Info color={theme.colors.blue[7]} weight="fill" />
-                                        </Group>
-                                    ),
-                                },
-                                {
-                                    accessor: 'permission',
-                                    sortable: false,
-                                    textAlign: 'left',
-                                    title: (
-                                        <Group gap="xs">
-                                            <Text>Permission</Text>
-                                            <Info color={theme.colors.blue[7]} weight="fill" />
-                                        </Group>
-                                    ),
-                                },
-                                { accessor: 'last-active', sortable: false, textAlign: 'left' },
-                            ]}
-                            sortIcons={{
-                                sorted: <ArrowDown color={theme.colors.charcoal[7]} />,
-                                unsorted: <ArrowUp color={theme.colors.charcoal[7]} />,
-                            }}
-                        />
-                    </Stack>
-                </Paper>
-            </Stack>
-        </>
+        <Stack p="xl">
+            <AdminBreadcrumbs crumbs={{ current: 'Manage team' }}></AdminBreadcrumbs>
+            <Title order={1}>Manage Team</Title>
+            <Paper shadow="xs" p="xl">
+                <Stack>
+                    <Group justify="space-between">
+                        <Title order={3}>People</Title>
+                        <Flex justify="flex-end">
+                            <Link href="/admin/invite">
+                                <Button leftSection={<Plus />}>Invite People</Button>
+                            </Link>
+                        </Flex>
+                    </Group>
+                    <Divider c="charcoal.1" />
+                    <DataTable
+                        withColumnBorders
+                        striped
+                        backgroundColor="charcoal.1"
+                        idAccessor="slug"
+                        noRecordsText="No organisations yet, add some using button below"
+                        noRecordsIcon={<Users />}
+                        records={sortedMembers}
+                        sortStatus={sortStatus}
+                        onSortStatusChange={setSortStatus}
+                        columns={[
+                            { accessor: 'name', sortable: true },
+                            {
+                                accessor: 'role',
+                                sortable: false,
+                                title: (
+                                    <Group gap="xs">
+                                        <Text>Role</Text>
+                                        <Info color={theme.colors.blue[7]} weight="fill" />
+                                    </Group>
+                                ),
+                            },
+                            {
+                                accessor: 'permission',
+                                sortable: false,
+                                textAlign: 'left',
+                                title: (
+                                    <Group gap="xs">
+                                        <Text>Permission</Text>
+                                        <Info color={theme.colors.blue[7]} weight="fill" />
+                                    </Group>
+                                ),
+                            },
+                            { accessor: 'last-active', sortable: false, textAlign: 'left' },
+                        ]}
+                        sortIcons={{
+                            sorted: <ArrowDown color={theme.colors.charcoal[7]} />,
+                            unsorted: <ArrowUp color={theme.colors.charcoal[7]} />,
+                        }}
+                    />
+                </Stack>
+            </Paper>
+        </Stack>
     )
 }
