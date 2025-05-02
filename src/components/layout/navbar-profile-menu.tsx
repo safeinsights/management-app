26d11c61f1687a13485a13ef8c43f56257414d73
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2025-05-02 15:06:11 -0500

    add menu options for profile entry. create collapsable menu component

--- src/components/layout/navbar-profile-menu.tsx
+++ src/components/layout/navbar-profile-menu.tsx
@@ -3,13 +3,47 @@
 import { AppShellSection, NavLink } from '@mantine/core'
 import { UserAvatar } from '@/components/user-avatar'
 import { UserName } from '@/components/user-name'
-import { CaretRight } from '@phosphor-icons/react/dist/ssr'
+import { CaretRight, Lock, SignOut, User } from '@phosphor-icons/react/dist/ssr'
 import styles from './navbar-items.module.css'
+import { CollapseContainer } from './items-collapse'
+import { useDisclosure } from '@mantine/hooks'
+import { useClerk } from '@clerk/nextjs'
 
 export function NavbarProfileMenu() {
+    const { signOut, openUserProfile } = useClerk()
+    const [opened, { toggle }] = useDisclosure()
+
     return (
         <AppShellSection>
-            {/* Placeholder for collapsable menu */}
+            <CollapseContainer opened={opened}>
+                <NavLink
+                    label="My Account"
+                    leftSection={<User />}
+                    c="white"
+                    className={styles.navLinkProfileHover}
+                    onClick={() => {
+                        openUserProfile()
+                    }}
+                />
+                <NavLink
+                    label="Reviewer Key"
+                    leftSection={<Lock />}
+                    onClick={() => {
+                        window.alert('404. Design under construction')
+                    }}
+                    c="white"
+                    className={styles.navLinkProfileHover}
+                />
+                <NavLink
+                    label="Sign Out"
+                    leftSection={<SignOut />}
+                    onClick={() => {
+                        signOut()
+                    }}
+                    c="white"
+                    className={styles.navLinkProfileHover}
+                />
+            </CollapseContainer>
             <NavLink
                 label={
                     <>
@@ -20,6 +54,7 @@ export function NavbarProfileMenu() {
                 rightSection={<CaretRight />}
                 c="white"
                 className={styles.navLinkProfileHover}
+                onClick={toggle}
             />
         </AppShellSection>
     )
