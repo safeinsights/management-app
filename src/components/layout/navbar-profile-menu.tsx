38cb124a223a2eb20551783429f174935caa9987
Chris Bendel <chrissbendel@gmail.com>
2025-05-02 14:02:11 -0400

    Lint

--- src/components/layout/navbar-profile-menu.tsx
+++ src/components/layout/navbar-profile-menu.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { NavLink, AppShellSection } from '@mantine/core'
+import { AppShellSection, NavLink } from '@mantine/core'
 import { UserAvatar } from '@/components/user-avatar'
 import { UserName } from '@/components/user-name'
 import { CaretRight } from '@phosphor-icons/react/dist/ssr'
-import { useUser } from '@clerk/nextjs'
 import styles from './navbar-items.module.css'
 
 export function NavbarProfileMenu() {
