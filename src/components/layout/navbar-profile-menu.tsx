284e71944877c84feea509107d7b434450c26aa3
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2025-05-04 16:28:07 -0500

    removed collapse component wrapper and opted for inline use in the profile-menu instead

--- src/components/layout/navbar-profile-menu.tsx
+++ src/components/layout/navbar-profile-menu.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { AppShellSection, NavLink } from '@mantine/core'
+import { AppShellSection, Collapse, NavLink } from '@mantine/core'
 import { UserAvatar } from '@/components/user-avatar'
 import { UserName } from '@/components/user-name'
 import { CaretRight, Lock, SignOut, User } from '@phosphor-icons/react/dist/ssr'
 import styles from './navbar-items.module.css'
-import { CollapseContainer } from './items-collapse'
 import { useDisclosure } from '@mantine/hooks'
 import { useClerk } from '@clerk/nextjs'
 
@@ -15,7 +14,7 @@ export function NavbarProfileMenu() {
 
     return (
         <AppShellSection>
-            <CollapseContainer opened={opened}>
+            <Collapse in={opened} bg={'var(--mantine-color-purple-9)'}>
                 <NavLink
                     label="My Account"
                     leftSection={<User />}
@@ -43,7 +42,7 @@ export function NavbarProfileMenu() {
                     c="white"
                     className={styles.navLinkProfileHover}
                 />
-            </CollapseContainer>
+            </Collapse>
             <NavLink
                 label={
                     <>
