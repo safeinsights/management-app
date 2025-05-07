5871f61eb70cee706f7c08daef37f511b436445f
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-05-07 22:57:19 +0300

    improve looks of OrganizationSwitcher more

--- src/components/layout/navbar-items.tsx
+++ src/components/layout/navbar-items.tsx
@@ -99,23 +99,29 @@ export const NavbarItems: FC = () => {
                 </Protect>
             )}
 
-            <Group justify="left" c="white">
+            <Group justify="left" c="white" w="100%">
                 <OrganizationSwitcher
                     afterSelectOrganizationUrl="/"
                     appearance={{
                         elements: {
-                            root: {
+                            rootBox: {
                                 width: '100%',
                             },
                             organizationSwitcherTrigger: {
                                 color: 'white !important',
                                 '& span': { color: 'white !important' },
-                                padding: `var(--mantine-spacing-xs) var(--mantine-spacing-sm)`,
+                                padding: `12px 10px`,
                                 width: '100%',
                                 borderRadius: '0',
                                 '&:hover': {
                                     backgroundColor: 'var(--mantine-color-blue-9)',
                                 },
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                            },
+                            organizationPreview: {
+                                gap: 'var(--mantine-spacing-sm)',
                             },
                         },
                     }}
