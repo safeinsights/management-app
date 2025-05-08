2f36e4bba6f4b81b59e07ca63ca33b7a5ae86e20
Nathan Stitt <nathan@stitt.org>
2025-05-08 11:40:16 -0500

    revert change to id col

--- src/database/migrations/1746319371011_pending_users.ts
+++ src/database/migrations/1746319371011_pending_users.ts
@@ -4,14 +4,14 @@ export async function up(db: Kysely<unknown>): Promise<void> {
     await db.schema
         .createTable('pending_user')
         .addColumn('id', 'uuid', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
-        .addColumn('org_id', 'uuid', (col) => col.references('org.id').notNull())
+        .addColumn('organization_id', 'uuid', (col) => col.references('org.id').notNull())
         .addColumn('email', 'text', (col) => col.notNull())
         .addColumn('isResearcher', 'boolean', (col) => col.notNull())
         .addColumn('isReviewer', 'boolean', (col) => col.notNull())
         .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
         .execute()
 
-    await db.schema.createIndex('pending_user_org_id_idx').on('pending_user').column('org_id').execute()
+    await db.schema.createIndex('pending_user_org_id_idx').on('pending_user').column('organization_id').execute()
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
