5dc9f9f101dfc8dfea5db0c1dfc157ba8b24fece
Stella Tetradis <stella.tetradis@gmail.com>
2025-05-08 13:44:10 -0400

    add success message to modal

--- src/app/organization/[orgSlug]/admin/invitation.tsx
+++ src/app/organization/[orgSlug]/admin/invitation.tsx
@@ -1,24 +1,40 @@
 'use client'
 
-import { Divider, TextInput, Button, Flex, Radio } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { Divider, TextInput, Button, Flex, Radio, useMantineTheme, Anchor, Text } from '@mantine/core'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useForm } from '@mantine/form'
-import { notifications } from '@mantine/notifications'
 import { orgAdminInviteUserAction } from './admin-users.actions'
 import { InviteUserFormValues, inviteUserSchema } from './invite-user.schema'
 import { InputError, reportMutationError } from '@/components/errors'
-import { Plus } from '@phosphor-icons/react/dist/ssr'
+import { CheckCircle, Plus } from '@phosphor-icons/react/dist/ssr'
 import { AppModal } from '@/components/modal'
 import { zodResolver } from 'mantine-form-zod-resolver'
-import { FC } from 'react'
-import { useDisclosure } from '@mantine/hooks'
+import { type FC, useState } from 'react'
+
 import { PendingUsers } from './pending-invites'
 
 interface InviteFormProps {
     orgSlug: string
 }
 
-export const InviteForm: FC<InviteFormProps> = ({ orgSlug }) => {
+const InviteSuccess: FC<{ onContinue: () => void }> = ({ onContinue }) => {
+    const theme = useMantineTheme()
+
+    return (
+        <Flex direction="column" justify="center" align="center" gap="xs" mb="sm" fw="semibold">
+            <CheckCircle size={28} color={theme.colors.green[9]} weight="fill" />
+            <Text c="green.9" size="md" fw="bold">
+                Invitation sent successfully!
+            </Text>
+            <Anchor component="button" mt={16} size="sm" c="blue.8" fw={600} onClick={onContinue}>
+                Continue to invite people
+            </Anchor>
+        </Flex>
+    )
+}
+
+const InviteForm: FC<{ orgSlug: string; onInvited: () => void }> = ({ orgSlug, onInvited }) => {
     const queryClient = useQueryClient()
 
     const studyProposalForm = useForm({
@@ -32,26 +48,25 @@ export const InviteForm: FC<InviteFormProps> = ({ orgSlug }) => {
 
     const { mutate: inviteUser, isPending: isInviting } = useMutation({
         mutationFn: (invite: InviteUserFormValues) => orgAdminInviteUserAction({ invite, orgSlug }),
-        onError: reportMutationError,
+        onError: (error) => {
+            reportMutationError(error)
+        },
         onSuccess() {
-            notifications.show({
-                message: 'User invited successfully',
-                color: 'green',
-            })
             studyProposalForm.reset()
             queryClient.invalidateQueries({ queryKey: ['pendingUsers', orgSlug] })
+            onInvited()
         },
     })
 
-    const onSubmit = studyProposalForm.onSubmit((values) =>
-        inviteUser({
-            ...values,
-            role: values.role as any, // eslint-disable-line @typescript-eslint/no-explicit-any
-        }),
-    )
-
     return (
-        <form onSubmit={onSubmit}>
+        <form
+            onSubmit={studyProposalForm.onSubmit((values) =>
+                inviteUser({
+                    ...values,
+                    role: values.role as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+                }),
+            )}
+        >
             <TextInput
                 label="Invite by email"
                 placeholder="Enter email address"
@@ -65,11 +80,7 @@ export const InviteForm: FC<InviteFormProps> = ({ orgSlug }) => {
             <Flex mb="sm" fw="semibold">
                 <Radio.Group
                     label="Assign Role"
-
-                    styles={{
-                        error: { fontWeight: 600, marginTop: 10 },
-                        label: { fontWeight: 600, marginBottom: 4 },
-                    }}
+                    styles={{ label: { fontWeight: 600, marginBottom: 4 } }}
                     name="role"
                     {...studyProposalForm.getInputProps('role', { type: 'checkbox' })}
                 >
@@ -81,18 +92,32 @@ export const InviteForm: FC<InviteFormProps> = ({ orgSlug }) => {
                 </Radio.Group>
             </Flex>
 
-            <Button type="submit" mt="sm" loading={isInviting}>
+            <Button type="submit" mt="sm" loading={isInviting} disabled={!studyProposalForm.isValid()}>
                 Send invitation
             </Button>
-
-            <Divider c="charcoal.1" my="xl" />
-
-            <PendingUsers orgSlug={orgSlug} />
         </form>
     )
 }
 
-export const InviteButton: React.FC<{ orgSlug: string }> = ({ orgSlug }) => {
+const InvitePanel: FC<InviteFormProps> = ({ orgSlug }) => {
+    const [wasInvited, setWasInvited] = useState(false)
+
+    const body = wasInvited ? (
+        <InviteSuccess onContinue={() => setWasInvited(false)} />
+    ) : (
+        <InviteForm orgSlug={orgSlug} onInvited={() => setWasInvited(true)} />
+    )
+
+    return (
+        <>
+            {body}
+            <Divider c="charcoal.1" my="xl" />
+            <PendingUsers orgSlug={orgSlug} />
+        </>
+    )
+}
+
+export const InviteButton: FC<{ orgSlug: string }> = ({ orgSlug }) => {
     const [inviteUserOpened, { open: openInviteUser, close: closeInviteUser }] = useDisclosure(false)
 
     return (
@@ -103,7 +128,7 @@ export const InviteButton: React.FC<{ orgSlug: string }> = ({ orgSlug }) => {
                 title="Invite others to join your team"
                 size="lg"
             >
-                <InviteForm orgSlug={orgSlug} />
+                <InvitePanel orgSlug={orgSlug} />
             </AppModal>
 
             <Button leftSection={<Plus />} onClick={openInviteUser}>
