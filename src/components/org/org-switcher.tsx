6e169a5ff189248fa0311ff9f6e2369e7e5a6040
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2025-05-08 15:40:46 -0500

    linting

--- src/components/org/org-switcher.tsx
+++ src/components/org/org-switcher.tsx
@@ -12,31 +12,31 @@ export const OrgSwitcher: FC = () => {
     return (
         <Group justify="left" pl="xs" c="white">
             <OrganizationSwitcher
-                    afterSelectOrganizationUrl="/"
-                    appearance={{
-                        elements: {
-                            rootBox: {
-                                width: '100%',
-                            },
-                            organizationSwitcherTrigger: {
-                                color: 'white !important',
-                                '& span': { color: 'white !important' },
-                                padding: `12px 10px`,
-                                width: '100%',
-                                borderRadius: '0',
-                                '&:hover': {
-                                    backgroundColor: 'var(--mantine-color-blue-9)',
-                                },
-                                display: 'flex',
-                                justifyContent: 'space-between',
-                                alignItems: 'center',
-                            },
-                            organizationPreview: {
-                                gap: 'var(--mantine-spacing-sm)',
-                            },
+                afterSelectOrganizationUrl="/"
+                appearance={{
+                    elements: {
+                        rootBox: {
+                            width: '100%',
                         },
-                    }}
-                />
+                        organizationSwitcherTrigger: {
+                            color: 'white !important',
+                            '& span': { color: 'white !important' },
+                            padding: `12px 10px`,
+                            width: '100%',
+                            borderRadius: '0',
+                            '&:hover': {
+                                backgroundColor: 'var(--mantine-color-blue-9)',
+                            },
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        },
+                        organizationPreview: {
+                            gap: 'var(--mantine-spacing-sm)',
+                        },
+                    },
+                }}
+            />
         </Group>
     )
 }
