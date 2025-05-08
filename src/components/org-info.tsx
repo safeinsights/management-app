0b0e89fd364924b6653d940c1e15db0291d3bbce
Nathan Stitt <nathan@stitt.org>
2025-05-08 15:06:40 -0500

    orgs may be undefined

--- src/components/org-info.tsx
+++ src/components/org-info.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'next/navigation'
 
 export function useHasMultipleRoles() {
     const { user } = useUser()
-    const dualRoleOrg = user?.publicMetadata?.orgs.find((org) => org.isAdmin || (org.isResearcher && org.isReviewer))
+    const dualRoleOrg = user?.publicMetadata?.orgs?.find((org) => org.isAdmin || (org.isResearcher && org.isReviewer))
     return !!dualRoleOrg
 }
 
@@ -12,7 +12,7 @@ export function useOrgInfo(orgSlugFallback = '') {
 
     const { orgSlug: paramOrgSlug } = useParams<{ orgSlug: string }>()
 
-    const org = user?.publicMetadata?.orgs.find((org) => org.slug == (paramOrgSlug || orgSlugFallback))
+    const org = user?.publicMetadata?.orgs?.find((org) => org.slug == (paramOrgSlug || orgSlugFallback))
 
     return {
         isLoaded,
