b8aad986932739e869d5b138513d1235185d47bd
Nathan Stitt <nathan@stitt.org>
2025-05-08 11:32:04 -0500

    remove old files

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -5,16 +5,34 @@ import { createTempDir } from '@/tests/unit.helpers'
 import fs from 'fs'
 import { ClerkProvider, useClerk } from '@clerk/nextjs'
 import { cleanup } from '@testing-library/react'
+//import { AsyncLocalStorage } from 'node:async_hooks'
 
 const Headers = new Map()
+
+// type NextRequestContext = {
+//   get(): NextRequestContextValue | undefined
+// }
+
+// type NextRequestContextValue = {
+//   waitUntil?: (promise: Promise<any>) => void
+// }
+
+// const RequestContextStorage = new AsyncLocalStorage<NextRequestContextValue>()
+
+// // Define and inject the accessor that next.js will use
+// const RequestContext: NextRequestContext = {
+//   get() {
+//     return RequestContextStorage.getStore()
+//   },
+// }
+// globalThis[Symbol.for('@next/request-context')] = RequestContext
 
 beforeAll(async () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     vi.mock('next/router', () => require('next-router-mock'))
     vi.mock('next/server', async (importOriginal) => ({
-        ...await importOriginal(),
-        after: (cb: () => void) => cb()
-
+        ...(await importOriginal()),
+        after: (cb: () => void) => cb(),
     }))
 
     // https://github.com/scottrippey/next-router-mock/issues/67#issuecomment-1564906960
