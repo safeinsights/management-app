968c4351190c058156b270f3ca5988e7283e3b41
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-05-16 02:01:34 +0300

    show errors on name textbox validation

--- src/app/organization/[orgSlug]/admin/settings/organization-settings-manager.tsx
+++ src/app/organization/[orgSlug]/admin/settings/organization-settings-manager.tsx
@@ -31,6 +31,7 @@ export function OrganizationSettingsManager({
             description: initialDescription || '',
         },
         validate: zodResolver(settingsFormSchema),
+        validateInputOnBlur: true,
     })
 
     useEffect(() => {
