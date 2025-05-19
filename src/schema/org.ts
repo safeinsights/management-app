5ab71c9141321d4d3238bb92bf8f91ad86b07618
Nathan Stitt <nathan@stitt.org>
2025-05-18 21:07:34 -0700

    allow digits in slug

--- src/schema/org.ts
+++ src/schema/org.ts
@@ -3,7 +3,7 @@ import { Selectable } from 'kysely'
 import { Org as DefinedOrg } from '@/database/types'
 
 export const orgSchema = z.object({
-    slug: z.string().regex(/^[a-z][a-z\-]*[a-z]$/, { message: 'Invalid slug, all lowercase, only dashes' }),
+    slug: z.string().regex(/^[a-z][0-9][a-z\-]*[a-z]$/, { message: 'Invalid slug must be all lowercase alphanumeric or dashes' }),
     name: z.string().min(1, { message: 'Name must be provided' }),
     email: z.string().email({ message: 'Invalid email address' }),
     publicKey: z.string().min(1, { message: 'PubKey cannot be blank' }),
