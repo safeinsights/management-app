28065efff8f3c0e8c0ef9c190e09ab498b257375
Nathan Stitt <nathan@stitt.org>
2025-05-22 12:45:03 -0700

    make server layout; show error when public key missing

--- src/components/layout/anon-layout.tsx
+++ src/components/layout/anon-layout.tsx
@@ -1,3 +1,6 @@
+'use server'
+
+import { ErrorAlert } from '../errors'
 import { AnonLayoutShell } from './anon-layout-shell'
 import { ClerkProvider } from '@clerk/nextjs'
 
@@ -5,8 +8,10 @@ type Props = {
     children: React.ReactNode
 }
 
-export function AnonLayout({ children }: Props) {
+
+export async function AnonLayout({ children }: Props) {
     const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || ''
+    if (!clerkPublishableKey) return <ErrorAlert error={"missing clerk key"} />
 
     return (
         <ClerkProvider publishableKey={clerkPublishableKey}>
