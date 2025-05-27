61c02e13691a713eae86c37a530e22cbb5ead7e3
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-05-28 01:12:41 +0300

    make error more human readable

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -94,7 +94,7 @@ export const errorToString = (error: unknown) => {
     const actionFailure = extractActionFailure(error)
     if (actionFailure) {
         return Object.entries(actionFailure)
-            .map(([field, msg]) => `${capitalize(field)} ${msg}`)
+            .map(([field, msg]) => `${capitalize(field)}: ${msg}`)
             .join(', ')
     }
 
