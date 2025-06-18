1fddad62de1c00d780b862d99cc826e9e17eca37
Nathan Stitt <nathan@stitt.org>
2025-06-18 16:31:25 -0500

    cleanup commented out code

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -49,24 +49,7 @@ export const minimalJobInfoSchema = minimalStudyInfoSchema.extend({
 })
 export type MinimalJobInfo = z.infer<typeof minimalJobInfoSchema>
 
-// there's probably a way to do this with zod schema
-// but don't need it yet so i haven't investigated how
-// export type MinimalJobResultsInfo = MinimalJobInfo &
-//     (
-//         | {
-//               resultsType: 'APPROVED'
-//               resultsPath: string
-//           }
-//         | {
-//               resultsType: 'ENCRYPTED'
-//           }
-//     )
-
 export type AllStatus = StudyJobStatus | StudyStatus
-
-// export function isMinimalStudyJobInfo(info: MinimalStudyInfo | MinimalJobInfo): info is MinimalJobInfo {
-//     return 'studyJobId' in info
-// }
 
 export const CLERK_ADMIN_ORG_SLUG = 'safe-insights' as const
 
--- src/server/storage.ts
+++ src/server/storage.ts
@@ -14,16 +14,6 @@ export async function fetchFileContents(filePath: string) {
     return new Blob(chunks)
 }
 
-// export async function fetchStudyApprovedResultsFile(info: MinimalJobInfo) {
-//     const file = await getStudyJobFileOfType(info.studyJobId, 'APPROVED_RESULT')
-//     return await fetchFile(pathForStudyJobFile(info, file))
-// }
-
-// export async function fetchStudyEncryptedResultsFile(info: MinimalJobInfo) {
-//     const file = await getStudyJobFileOfType(info.studyJobId, 'ENCRYPTED_RESULT')
-//     return await fetchFile(pathForStudyJobFile(info, file))
-// }
-
 export async function urlForFile(filePath: string): Promise<string> {
     return await signedUrlForFile(filePath)
 }
@@ -32,23 +22,9 @@ export async function urlForStudyJobCodeFile(info: MinimalJobInfo, fileName: str
     return urlForFile(pathForStudyJobCodeFile(info, fileName))
 }
 
-// export async function urlForResultsFile(info: MinimalJobResultsInfo): Promise<string> {
-//     const filePath = pathForStudyJobResults(info)
-//     return urlForFile(filePath)
-// }
-
 export async function urlForStudyDocumentFile(info: MinimalStudyInfo, fileType: StudyDocumentType, fileName: string) {
     return urlForFile(pathForStudyDocumentFile(info, fileType, fileName))
 }
-
-// export async function fetchStudyCodeFile(info: MinimalJobInfo, filePath: string) {
-//     return await fetchFile(pathForStudyJobCodeFile(info, filePath))
-// }
-
-// export async function fetchCodeManifest(info: MinimalJobInfo) {
-//     const body = await fetchStudyCodeFile(info, 'manifest.json')
-//     return JSON.parse(await body.text()) as CodeManifest
-// }
 
 async function storeJobFile(info: MinimalJobInfo, path: string, file: File, fileType: FileType) {
     await storeS3File(info, file.stream(), path)
@@ -66,7 +42,3 @@ export async function storeStudyEncryptedResultsFile(info: MinimalJobInfo, file:
 export async function storeStudyApprovedResultsFile(info: MinimalJobInfo, file: File) {
     return await storeJobFile(info, `${pathForStudyJob(info)}/results/approved/${file.name}`, file, 'APPROVED-RESULT')
 }
-
-// export async function fetchStudyResultsFile(info: MinimalJobResultsInfo) {
-//     return await fetchFile(pathForStudyJobResults(info))
-// }
