29cea0408ae949d557fd86fcb6650bd6fa34fcc4
Nathan Stitt <nathan@stitt.org>
2025-06-24 13:28:48 -0500

    docker from url isn't quite a url

--- src/app/admin/team/[orgSlug]/settings/base-images.schema.ts
+++ src/app/admin/team/[orgSlug]/settings/base-images.schema.ts
@@ -4,6 +4,6 @@ export const orgBaseImageSchema = z.object({
     name: z.string(),
     cmdLine: z.string(),
     language: z.enum(['R'], { message: 'Language must be R' }),
-    url: z.string().url('Must be a valid URL').min(1, 'URL is required'),
+    url: z.string(),
     isTesting: z.boolean().default(false),
 })
