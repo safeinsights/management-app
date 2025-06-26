b899eaf52dac679a39a7f8c3ff33e44960fd31bd
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-06-26 22:39:57 +0200

    try to find sign out button differently by label e2e

--- tests/org-admin.spec.ts
+++ tests/org-admin.spec.ts
@@ -54,7 +54,7 @@ test.describe('Organization Admin', () => {
         await page.waitForURL('**/admin/team/openstax')
         await page.getByRole('button', { name: 'Toggle profile menu' }).click()
 
-        const signOutBtn = page.getByRole('button', { name: 'Sign Out' })
+        const signOutBtn = page.getByLabel('Sign Out')
         await expect(signOutBtn).toBeVisible() // wait for menu content to render
         await signOutBtn.click()
         await page.waitForURL('**/signin**')
