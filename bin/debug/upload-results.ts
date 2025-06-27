835753a85a5af8c93ec33c278da6e03c6282258b
Chris Bendel <chrissbendel@gmail.com>
2025-06-27 12:43:20 -0400

    Note

--- bin/debug/upload-results.ts
+++ bin/debug/upload-results.ts
@@ -4,6 +4,7 @@ import { DebugRequest } from './request'
 import { ResultsWriter } from 'si-encryption/job-results/writer'
 import { pemToArrayBuffer, fingerprintKeyData } from 'si-encryption/util/keypair'
 
+// TODO Just for testing reference, remove on final PR
 // npx tsx bin/debug/upload-results.ts -j 0197a365-aeb3-7c3e-b43f-b6c251a494ef -r tests/assets/results-with-pii.csv -l tests/assets/error-log.txt
 // npx tsx bin/debug/upload-results.ts -j 0197a365-aeb3-7c3e-b43f-b6c251a494ef -l tests/assets/error-log.txt
 
