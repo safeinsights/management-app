fa16e491dadc75413dfb1bd7f1e4b3847e568775
KC Swanson <kcs7@rice.edu>
2025-07-01 11:16:34 -0400

    remove unused vars

--- src/app/api/job/[jobId]/status/route.ts
+++ src/app/api/job/[jobId]/status/route.ts
@@ -1,17 +1,8 @@
 export const dynamic = 'force-dynamic' // defaults to auto
 import { db } from '@/database'
-import { z } from 'zod'
 import { NextResponse } from 'next/server'
 import { wrapApiOrgAction } from '@/server/api-wrappers'
 import { apiRequestingOrg } from '@/server/api-context'
-
-const schema = z.object({
-    message: z.string().optional(),
-    // it's tempting to try to type this, but doesn't seem to work.
-    // not really needed though because the where clause below will error if an invalid status is present in the list below
-    // TODO: consider removing the RESULTS status since we're reviewing in the BMA now
-    status: z.enum(['JOB-PROVISIONING', 'JOB-RUNNING', 'JOB-ERRORED', 'RESULTS-REJECTED', 'RESULTS-APPROVED']),
-})
 
 const handler = async (req: Request, { params }: { params: Promise<{ jobId: string }> }) => {
     const org = apiRequestingOrg()
@@ -27,13 +18,10 @@ const handler = async (req: Request, { params }: { params: Promise<{ jobId: stri
         .select(['status', 'message'])
         .executeTakeFirst()
 
-    // console.log('status', status)
-
     if (!status) {
         return new NextResponse('Not found', { status: 404 })
     }
 
-    // return new NextResponse(, { status: 200 })
     return Response.json(status)
 }
 
