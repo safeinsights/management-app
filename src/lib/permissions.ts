81128b0647337a6d3cdcb085cf389c916d51c120
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-07-25 00:53:37 +0300

    add permissions for SI admins

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -74,6 +74,10 @@ export function defineAbilityFor(session: UserSession) {
         permit('read', 'Team')
         permit('update', 'Team')
         permit('delete', 'Team')
+
+        permit('invite', 'User')
+        permit('update', 'User')
+        permit('read', 'User')
     }
 
     return build()
