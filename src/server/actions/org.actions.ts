b0354de6cfc2ab16aa8b8a8bef920237b38dc70d
therealmarv <1050582+therealmarv@users.noreply.github.com>
2025-07-25 01:07:40 +0300

    add middleware to pull teamSlug and teamId from session

--- src/server/actions/org.actions.ts
+++ src/server/actions/org.actions.ts
@@ -91,15 +91,14 @@ export const getUsersForOrgAction = new Action('getUsersForOrgAction')
         }),
     )
 
-    .requireAbilityTo(
-        'read',
-        'User',
-        async ({ orgSlug }, { session }) => {
-            if (orgSlug !== session.team.slug) throw new ActionFailure({ orgSlug: 'mismatch' })
-            return { orgId: session.team.id }
-        },
-    )
-
+    .middleware(async (_, { session }) => {
+        if (!session) throw new ActionFailure({ user: 'Unauthorized' })
+        return { teamSlug: session.team.slug, teamId: session.team.id }
+    })
+    .requireAbilityTo('read', 'User', async ({ orgSlug }, { teamSlug, teamId }) => {
+        if (orgSlug !== teamSlug) throw new ActionFailure({ orgSlug: 'mismatch' })
+        return { orgId: teamId }
+    })
     .handler(async ({ orgSlug, sort }) => {
         return await db
             .selectFrom('orgUser')
