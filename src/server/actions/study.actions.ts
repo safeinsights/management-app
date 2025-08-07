046ae2158c345e7a3d0efa94b7d92d1938541c7c
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2025-08-07 12:41:58 -0500

    fix: remove duplicate parameter definition in approveStudyProposalAction

--- src/server/actions/study.actions.ts
+++ src/server/actions/study.actions.ts
@@ -181,7 +181,6 @@ export const approveStudyProposalAction = new Action('approveStudyProposalAction
             studyId: z.string(),
             orgSlug: z.string(),
             useTestImage: z.boolean().optional(),
-            useTestImage: z.boolean().optional(),
         }),
     )
     .middleware(async ({ params: { studyId }, db }) => {
@@ -193,7 +192,6 @@ export const approveStudyProposalAction = new Action('approveStudyProposalAction
         return { study, orgId: study.orgId }
     })
     .requireAbilityTo('approve', 'Study')
-    .handler(async ({ params: { studyId, orgSlug, useTestImage }, study, session, db }) => {
     .handler(async ({ params: { studyId, orgSlug, useTestImage }, study, session, db }) => {
         const userId = session.user.id
 
