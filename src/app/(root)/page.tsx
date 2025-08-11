f39627b96573d2368a08ceed8c4ec80eb3a197b3
Stella Tetradis <stella.tetradis@gmail.com>
2025-08-11 13:58:50 -0400

    revert

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,30 +1,16 @@
-'use client'
-
-import DashboardSkeleton from '@/components/layout/skeleton/dashboard'
-import { useSession } from '@/hooks/session'
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { mainStyles, pageStyles } from '@/styles/common'
+import { UserNav } from './user-nav'
 
 export const dynamic = 'force-dynamic'
 
+// TODO Remove this root page?,
+//  or route users based on their roles to correct pages?
 export default function Home() {
-    const { isLoaded, session } = useSession()
-    const [isNavigating, setIsNavigating] = useState(false)
-    const router = useRouter()
-
-    useEffect(() => {
-        if (!session) return
-
-        setIsNavigating(true)
-        if (session.team.isResearcher) {
-            router.push('/researcher/dashboard')
-        } else if (session.team.isReviewer) {
-            router.push(`/reviewer/${session.team.slug}/dashboard`)
-        }
-    }, [session, router])
-
-    // Show dashboard skeleton while session is loading or during navigation
-    if (!isLoaded || isNavigating) {
-        return <DashboardSkeleton />
-    }
+    return (
+        <div className={pageStyles}>
+            <main className={mainStyles}>
+                <UserNav />
+            </main>
+        </div>
+    )
 }
---
+++ src/app/(root)/user-nav.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import DashboardSkeleton from '@/components/layout/skeleton/dashboard'
+import { useSession } from '@/hooks/session'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+export const UserNav = () => {
+    const { isLoaded, session } = useSession()
+    const [isNavigating, setIsNavigating] = useState(false)
+    const router = useRouter()
+
+    useEffect(() => {
+        if (!session) return
+
+        setIsNavigating(true)
+        if (session.team.isResearcher) {
+            router.push('/researcher/dashboard')
+        } else if (session.team.isReviewer) {
+            router.push(`/reviewer/${session.team.slug}/dashboard`)
+        }
+    }, [session, router])
+
+    // Show dashboard skeleton while session is loading or during navigation
+    if (!isLoaded || isNavigating) {
+        return <DashboardSkeleton />
+    }
+}
