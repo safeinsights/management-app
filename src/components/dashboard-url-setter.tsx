7b37a226f25ddd46df8197a2804bf94c969794a6
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2025-08-13 19:41:21 -0500

    Refactor DashboardUrlSetter

--- src/components/dashboard-url-setter.tsx
+++ src/components/dashboard-url-setter.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { setLastDashboardUrlAction } from '@/server/actions/session.actions'
 import { useUser } from '@clerk/nextjs'
 
 export const DashboardUrlSetter: React.FC<{ url: string }> = ({ url }) => {
     const { user } = useUser()
+    const lastSetUrl = useRef<string>('')
 
     useEffect(() => {
-        setLastDashboardUrlAction({ url }).then(() => {
-            user?.reload()
-        })
+        if (url !== lastSetUrl.current && user) {
+            lastSetUrl.current = url
+            setLastDashboardUrlAction({ url }).then(() => {
+                user.reload()
+            })
+        }
     }, [url, user])
-
     return null
 }
