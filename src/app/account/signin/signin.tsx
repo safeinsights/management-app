6651f2c1e3daf709e9e4059925590e629b9c6c14
Stella Tetradis <stella.tetradis@gmail.com>
2025-09-03 22:37:28 -0400

    add sigin reset

--- src/app/account/signin/signin.tsx
+++ src/app/account/signin/signin.tsx
@@ -2,7 +2,8 @@
 
 import { useSignIn } from '@clerk/nextjs'
 import { Container, Loader } from '@mantine/core'
-import { useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import { type MFAState } from './logic'
 import { RequestMFA } from './mfa'
 import { SignInForm } from './sign-in-form'
@@ -11,9 +12,22 @@ type SignInStep = 'form' | 'mfa'
 
 export function SignIn() {
     const { isLoaded } = useSignIn()
+    const searchParams = useSearchParams()
+    const router = useRouter()
 
     const [step, setStep] = useState<SignInStep>('form')
     const [mfaState, setMFAState] = useState<MFAState>(false)
+
+    // Reset the sign-in flow when ?restart param is present
+    useEffect(() => {
+        const restart = searchParams.get('restart')
+        if (restart) {
+            setMFAState(false)
+            setStep('form')
+            // Remove the param from the URL to avoid loops
+            router.replace('/account/signin')
+        }
+    }, [searchParams, router])
 
     if (!isLoaded) {
         return <Loader />
