58a382e10e7a33a1b1fa3fe6be7686f9ded4f33a
Stella Tetradis <stella.tetradis@gmail.com>
2025-10-01 10:26:16 -0400

    fix table style

--- src/app/dashboard/user-tables/researcher-user-studies-table.tsx
+++ src/app/dashboard/user-tables/researcher-user-studies-table.tsx
@@ -12,7 +12,6 @@ import {
     Divider,
     Flex,
     Group,
-    Paper,
     Stack,
     Table,
     TableTbody,
@@ -66,70 +65,63 @@ export const ResearcherUserStudiesTable = () => {
 
     if (!studies || studies.length === 0) {
         return (
-            <Paper shadow="xs" p="xxl">
-                <Stack align="center" gap="md">
-                    <Text>You haven&apos;t started a study yet</Text>
-                    <ButtonLink
-                        leftSection={<PlusIcon />}
-                        href={`/researcher/study/request/${primaryEnclaveOrg.slug}`}
-                        data-testid="propose-study"
-                    >
-                        Propose New Study
-                    </ButtonLink>
-                </Stack>
-            </Paper>
+            <Stack align="center" gap="md" p="xxl">
+                <Text>You haven&apos;t started a study yet</Text>
+                <ButtonLink
+                    leftSection={<PlusIcon />}
+                    href={`/researcher/study/request/${primaryEnclaveOrg.slug}`}
+                    data-testid="propose-study"
+                >
+                    Propose New Study
+                </ButtonLink>
+            </Stack>
         )
     }
 
     return (
-        <Paper shadow="xs" p="xxl">
-            <Stack>
-                <Group justify="space-between">
-                    <Title order={3}>Proposed Studies</Title>
-                    <Flex justify="flex-end">
-                        <ButtonLink
-                            leftSection={<PlusIcon />}
-                            href={`/researcher/study/request/${primaryEnclaveOrg.slug}`}
-                        >
-                            Propose New Study
-                        </ButtonLink>
-                    </Flex>
-                </Group>
-                <Divider c="charcoal.1" />
-                <Refresher isEnabled={Boolean(needsRefreshed)} refresh={refetch} isPending={isFetching} />
-                <Table layout="fixed" verticalSpacing="md" striped="even" highlightOnHover stickyHeader>
-                    <TableThead>
-                        <TableTr>
-                            <TableTh fw={600}>Study Name</TableTh>
-                            <TableTh fw={600}>Submitted On</TableTh>
-                            <TableTh fw={600}>Submitted To</TableTh>
-                            <TableTh fw={600}>Stage</TableTh>
-                            <TableTh fw={600}>Status</TableTh>
-                            <TableTh fw={600}>Study Details</TableTh>
+        <Stack pt="xl">
+            <Group justify="space-between">
+                <Title order={3}>Proposed Studies</Title>
+                <Flex justify="flex-end">
+                    <ButtonLink leftSection={<PlusIcon />} href={`/researcher/study/request/${primaryEnclaveOrg.slug}`}>
+                        Propose New Study
+                    </ButtonLink>
+                </Flex>
+            </Group>
+            <Divider c="charcoal.1" />
+            <Refresher isEnabled={Boolean(needsRefreshed)} refresh={refetch} isPending={isFetching} />
+            <Table layout="fixed" verticalSpacing="md" striped="even" highlightOnHover stickyHeader>
+                <TableThead>
+                    <TableTr>
+                        <TableTh fw={600}>Study Name</TableTh>
+                        <TableTh fw={600}>Submitted On</TableTh>
+                        <TableTh fw={600}>Submitted To</TableTh>
+                        <TableTh fw={600}>Stage</TableTh>
+                        <TableTh fw={600}>Status</TableTh>
+                        <TableTh fw={600}>Study Details</TableTh>
+                    </TableTr>
+                </TableThead>
+                <TableTbody>
+                    {studies.map((study) => (
+                        <TableTr fz={14} key={study.id} bg={study.status === 'APPROVED' ? '#EAD4FC80' : undefined}>
+                            <TableTd>{study.title}</TableTd>
+                            <TableTd>{dayjs(study.createdAt).format('MMM DD, YYYY')}</TableTd>
+                            <TableTd>{study.orgName}</TableTd>
+                            <TableTd>{getStudyStage(study.status, 'researcher')}</TableTd>
+                            <TableTd>
+                                <DisplayStudyStatus
+                                    studyStatus={study.status}
+                                    audience="researcher"
+                                    jobStatusChanges={study.jobStatusChanges}
+                                />
+                            </TableTd>
+                            <TableTd>
+                                <Link href={`/researcher/study/${study.id}/review`}>View</Link>
+                            </TableTd>
                         </TableTr>
-                    </TableThead>
-                    <TableTbody>
-                        {studies.map((study) => (
-                            <TableTr fz={14} key={study.id} bg={study.status === 'APPROVED' ? '#EAD4FC80' : undefined}>
-                                <TableTd>{study.title}</TableTd>
-                                <TableTd>{dayjs(study.createdAt).format('MMM DD, YYYY')}</TableTd>
-                                <TableTd>{study.orgName}</TableTd>
-                                <TableTd>{getStudyStage(study.status, 'researcher')}</TableTd>
-                                <TableTd>
-                                    <DisplayStudyStatus
-                                        studyStatus={study.status}
-                                        audience="researcher"
-                                        jobStatusChanges={study.jobStatusChanges}
-                                    />
-                                </TableTd>
-                                <TableTd>
-                                    <Link href={`/researcher/study/${study.id}/review`}>View</Link>
-                                </TableTd>
-                            </TableTr>
-                        ))}
-                    </TableTbody>
-                </Table>
-            </Stack>
-        </Paper>
+                    ))}
+                </TableTbody>
+            </Table>
+        </Stack>
     )
 }
