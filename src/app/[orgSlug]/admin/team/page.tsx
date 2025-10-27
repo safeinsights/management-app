77d399158d3bb302d157d7c85528d7cea9edff3a
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2025-10-27 06:07:09 -0500

    Implement Research Lab Manage Team page and remove role references

---
+++ src/app/[orgSlug]/admin/team/page.tsx
@@ -0,0 +1,29 @@
+import { Paper, Stack, Title, Flex } from '@mantine/core'
+import { UsersTable } from './users-table'
+import { InviteButton } from './invitation'
+import { RequireOrgAdmin } from '@/components/require-org-admin'
+import { PageBreadcrumbs } from '@/components/page-breadcrumbs'
+
+export const dynamic = 'force-dynamic'
+
+export default async function UsersListingPage(props: { params: Promise<{ orgSlug: string }> }) {
+    const { orgSlug } = await props.params
+
+    return (
+        <Stack p="md">
+            <PageBreadcrumbs crumbs={[['Dashboard', `/`], [orgSlug, `/${orgSlug}`], ['Admin'], ['Manage team']]} />
+            <RequireOrgAdmin />
+            <Title my="lg">Manage team</Title>
+            <Paper shadow="xs" p="xl">
+                <Flex direction="row" justify={'space-between'} align="center">
+                    <Title order={3} mb="lg">
+                        People
+                    </Title>
+                    <InviteButton orgSlug={orgSlug} />
+                </Flex>
+
+                <UsersTable orgSlug={orgSlug} />
+            </Paper>
+        </Stack>
+    )
+}
