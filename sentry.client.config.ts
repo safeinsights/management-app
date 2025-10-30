18470bb13b469bb15fee8b409309fd63e78e4528
Stella Tetradis <stella.tetradis@gmail.com>
2025-10-29 21:52:08 -0400

    enable debug

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -38,9 +38,9 @@ Sentry.init({
     replaysOnErrorSampleRate: 1.0,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
+    debug: true,
 
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: true, // process.env.NODE_ENV === 'production',
 
     release: process.env.RELEASE_TAG || 'unknown',
     environment: process.env.ENVIRONMENT_ID || 'development',
