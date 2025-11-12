6af9fc3408d390e886433e5db3a1b6c0f0433590
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2025-11-12 16:17:53 -0600

    Update email delivery test to include researcher email as fallback

--- src/server/mailer.test.ts
+++ src/server/mailer.test.ts
@@ -37,7 +37,7 @@ describe('mailgun email functions', () => {
 
         expect(deliver).toHaveBeenCalledWith(
             expect.objectContaining({
-                to: expect.stringContaining(user1.email || ''),
+                to: expect.stringContaining(user1.email || researcher.email || ''),
                 bcc: expect.stringContaining(user1.email || ''),
                 subject: expect.stringContaining('New study proposal'),
                 template: 'vb - new research proposal',
