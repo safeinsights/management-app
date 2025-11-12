c816d802c38af29daeca33c389f1eebb12680705
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2025-11-12 16:35:22 -0600

    Remove error throw for missing reviewer email

--- src/server/mailer.ts
+++ src/server/mailer.ts
@@ -20,8 +20,6 @@ export const sendStudyProposalEmails = async (studyId: string) => {
 
     const emails = reviewersToNotify.map((reviewer) => reviewer.email).filter((email) => email)
 
-    if (!study.reviewerEmail) throw new Error(`no reviewer is set for studyId: ${studyId}`)
-
     await deliver({
         to: study.reviewerEmail ?? emails.join(', '), // work around in case no reviewer is set. mailgun requires a to address
         bcc: emails.join(', '),
