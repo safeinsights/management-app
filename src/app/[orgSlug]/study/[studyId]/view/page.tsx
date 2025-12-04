777be61b443c45b3b71ea8cabd95b1a06f573068
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2025-12-04 13:28:18 -0600

    fix test

--- src/app/[orgSlug]/study/[studyId]/view/page.tsx
+++ src/app/[orgSlug]/study/[studyId]/view/page.tsx
@@ -11,7 +11,6 @@ import { actionResult } from '@/lib/utils'
 import { extractJobStatus } from '@/hooks/use-job-results-status'
 import { StudyCodeDetails } from '@/components/study/study-code-details'
 import { OpenWorkspaceButton } from '@/components/study/open-workspace-button'
-import { NonProduction } from '@/components/non-production'
 
 export const dynamic = 'force-dynamic'
 
