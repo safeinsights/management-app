e084ace5929df334bc4e52d2ea822968e6622ec0
Stella Tetradis <stella.tetradis@gmail.com>
2026-01-28 12:30:30 -0500

    cleanup duplication

--- src/components/openstax-feature-flag.tsx
+++ src/components/openstax-feature-flag.tsx
@@ -29,19 +29,6 @@ interface OpenStaxFeatureFlagProps {
 // Swaps out the default content with the opt-in content based on the feature flag.
 // Use for swapping out entire components/large sections of code.
 export const OpenStaxFeatureFlag: FC<OpenStaxFeatureFlagProps> = ({ defaultContent, optInContent }) => {
-    const { isSpyMode } = useSpyMode()
-    const { isLoaded, session } = useSession()
-
-    if (!isLoaded) {
-        return defaultContent
-    }
-
-    const userOrgSlugs = Object.keys(session.orgs)
-    const belongsToOpenStax = userOrgSlugs.some((slug) =>
-        OPENSTAX_ORG_SLUGS.includes(slug as (typeof OPENSTAX_ORG_SLUGS)[number]),
-    )
-
-    const showOptIn = belongsToOpenStax && isSpyMode
-
-    return showOptIn ? optInContent : defaultContent
+    const isOpenStaxOptIn = useOpenStaxFeatureFlag()
+    return isOpenStaxOptIn ? optInContent : defaultContent
 }
