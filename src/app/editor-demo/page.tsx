31e4ce4707e244ea5b13c05848e07bacf3c92c7a
Nathan Stitt <nathan@stitt.org>
2026-03-18 20:59:32 -0500

    add DEPLOYED_ENV config and use it for editor WebSocket URL

--- src/app/editor-demo/page.tsx
+++ src/app/editor-demo/page.tsx
@@ -1,8 +1,8 @@
-import { PROD_ENV } from '@/server/config'
+import { DEPLOYED_ENV } from '@/server/config'
 import { EditorDemoLoader } from './editor-demo-loader'
 
 export default function EditorDemoPage() {
-    const wsUrl = PROD_ENV ? '/ws' : 'ws://localhost:1234'
+    const wsUrl = DEPLOYED_ENV ? '/ws' : 'ws://localhost:1234'
 
     return <EditorDemoLoader wsUrl={wsUrl} />
 }
