efa34b729a0092e07baeb2c352a0607ea6ebf6ae
Rodney Scott <92071426+Rod0352@users.noreply.github.com>
2026-04-12 16:47:15 -0500

    fix: improve code submission check logic in StudyAgreementsRoute

--- src/app/[orgSlug]/study/[studyId]/agreements/page.tsx
+++ src/app/[orgSlug]/study/[studyId]/agreements/page.tsx
@@ -31,8 +31,10 @@ export default async function StudyAgreementsRoute(props: {
     const isReviewer = currentOrg.type === 'enclave'
 
     if (isReviewer) {
-        const codeSubmitted = study.jobStatusChanges[0]?.status === 'CODE-SUBMITTED'
-        if (!codeSubmitted) {
+        const statuses = study.jobStatusChanges.map((s) => s.status)
+        const codeSubmitted = statuses.includes('CODE-SUBMITTED')
+        const codeAlreadyReviewed = statuses.some((s) => s === 'CODE-APPROVED' || s === 'CODE-REJECTED')
+        if (!codeSubmitted || codeAlreadyReviewed) {
             redirect(Routes.studyReview({ orgSlug, studyId }))
         }
 
