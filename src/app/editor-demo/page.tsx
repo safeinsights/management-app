5b1ded6e00c778d512e2a61d38bd0c212ea581ce
Stella Tetradis <stella.tetradis@gmail.com>
2026-04-19 16:42:02 -0400

    remove editor and error demo pages

--- src/app/editor-demo/page.tsx
+++
@@ -1,6 +0,0 @@
-import { WS_URL } from '@/server/config'
-import { EditorDemoLoader } from './editor-demo-loader'
-
-export default function EditorDemoPage() {
-    return <EditorDemoLoader wsUrl={WS_URL} />
-}
