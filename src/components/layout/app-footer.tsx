92b0574b3903e9d3ba875aa8d23de4c48274d59f
Stella Tetradis <stella.tetradis@gmail.com>
2026-06-01 14:59:20 -0400

    update footer

--- src/components/layout/app-footer.tsx
+++ src/components/layout/app-footer.tsx
@@ -1,12 +1,10 @@
-'use client'
-
 import { AppShellFooter, Text } from '@mantine/core'
 
 export function AppFooter() {
     return (
         <AppShellFooter p="md" bg="purple.9" bd="none">
             <Text ta="left" c="white" fz="sm">
-                © {new Date().getFullYear()} SafeInsights, Rice University
+                © {new Date().getFullYear()} - SafeInsights, Rice University
             </Text>
         </AppShellFooter>
     )
