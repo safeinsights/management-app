5c2d5374699911270a234a5a128f366bf44fa4bb
Stella Tetradis <stella.tetradis@gmail.com>
2026-07-21 15:11:54 -0400

    fix header

--- src/components/study/study-page-header.tsx
+++ src/components/study/study-page-header.tsx
@@ -1,10 +1,9 @@
 import type { ReactNode } from 'react'
-import { Title, useMantineTheme } from '@mantine/core'
+import { Title } from '@mantine/core'
 
 export function StudyPageHeader({ children }: { children: ReactNode }) {
-    const theme = useMantineTheme()
     return (
-        <Title order={1} fz={40} fw={700} c={theme.colors.charcoal[9]}>
+        <Title order={1} fz={40} fw={700} c="charcoal.9">
             {children}
         </Title>
     )
