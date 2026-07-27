1e15613a0d833a13c368a22469d2c0fc84753ae9
KC Swanson <kcs7@rice.edu>
2026-07-27 09:32:09 -0700

    Instantiate legal page

---
+++ src/app/admin/safeinsights/legal/page.tsx
@@ -0,0 +1,9 @@
+import { Stack, Title } from '@mantine/core'
+
+export default async function SafeInsightsLegalPage() {
+    return (
+        <Stack gap="xl">
+            <Title order={1}>SafeInsights Legal</Title>
+        </Stack>
+    )
+}
