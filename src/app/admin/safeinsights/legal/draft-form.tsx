790e49561db2af15772ce3c9e2c06811d5f16684
KC Swanson <kcs7@rice.edu>
2026-08-03 08:25:17 -0700

    save draft front end

---
+++ src/app/admin/safeinsights/legal/draft-form.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { useState } from '@/common'
+import { LegalDocumentType } from '@/database/types'
+import { uploadFiles } from '@/hooks/upload'
+import { isActionError } from '@/lib/errors'
+import { legalDocumentTypeLabels } from '@/schema/legal-document'
+import { createLegalDocumentDraftAction } from '@/server/actions/legal-document.actions'
+import { Paper, Title, Button, Flex, Group, Text, Stack } from '@mantine/core'
+import { Dropzone } from '@mantine/dropzone'
+import { UploadIcon, FileArrowUpIcon, ArrowCircleRightIcon } from '@phosphor-icons/react/dist/ssr'
+
+// Access modal from "Upload New {label}" button
+// Page 1: upload draft. Displays if there isn't a current draft
+// Page 2: Shows after finishing Page 1 ("Save Draft" button), or if there is a current draft
+// and contains review + publish logic
+
+export default function DraftForm({ doctype }: { doctype: LegalDocumentType }) {
+    const [file, setFile] = useState<File | null>(false)
+
+    const handleDrop = (files: File[]) => {
+        // Expects that there is one file and its type is .md
+        const draftFile = files[0]
+        setFile(draftFile)
+    }
+    const saveDraft = async () => {
+        // Call Chris's save draft action
+        const result = await createLegalDocumentDraftAction({
+            type: doctype,
+            fileName: file.name,
+            format: 'markdown',
+        })
+        if (isActionError(result)) {
+            throw new Error('Failed to create draft: ' + result)
+        }
+        // params: { type, orgId, studyId, fileName, format }
+        // Upload file to S3
+        await uploadFiles([[file, result.upload]])
+        console.log('file', file.name)
+    }
+    return (
+        <Stack>
+            <Title order={4} pb="sm">
+                Upload your draft document here:
+            </Title>
+            <Paper shadow="xs" p="md">
+                <Dropzone onDrop={handleDrop} p="md">
+                    <Group gap="xs" justify="center">
+                        <Dropzone.Accept>
+                            <UploadIcon size={24} />
+                        </Dropzone.Accept>
+                        <Dropzone.Idle>
+                            <FileArrowUpIcon size={24} />
+                        </Dropzone.Idle>
+                        <Text size="sm" c="dimmed">
+                            Drop files or click to browse
+                        </Text>
+                    </Group>
+                </Dropzone>
+                {file && <Text pt="sm">Uploaded: {file.name}</Text>}
+            </Paper>
+            <Flex align="right" justify="right">
+                <Button onClick={saveDraft} ml="xs" rightSection={<ArrowCircleRightIcon size={16} />}>
+                    Save Draft
+                </Button>
+            </Flex>
+        </Stack>
+    )
+}
--- src/app/admin/safeinsights/legal/page.tsx
+++ src/app/admin/safeinsights/legal/page.tsx
@@ -1,9 +1,12 @@
 import { Stack, Title } from '@mantine/core'
+import LegalUpload from './legal-upload'
 
 export default async function SafeInsightsLegalPage() {
     return (
         <Stack gap="xl">
             <Title order={1}>SafeInsights Legal</Title>
+            <LegalUpload doctype="tos" />
+            <LegalUpload doctype="pn" />
         </Stack>
     )
 }
