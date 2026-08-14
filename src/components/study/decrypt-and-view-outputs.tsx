e8df3c704302181b71bbe759782d6d744a93ce00
Stella Tetradis <stella.tetradis@gmail.com>
2026-08-14 11:40:15 -0400

    add shared helper

--- src/components/study/decrypt-and-view-outputs.tsx
+++ src/components/study/decrypt-and-view-outputs.tsx
@@ -1,10 +1,8 @@
 'use client'
 
 import { FC, useState } from 'react'
-import { FileOrImagePreviewModal } from '@/components/modals/file-or-image-preview-modal'
-import { OutputsFilesSection } from '@/components/study/outputs-files-section'
 import { SecurityKeyForm } from '@/components/study/security-key-form'
-import { useOutputsFiles } from '@/hooks/use-outputs-files'
+import { OutputsFilesViewer } from '@/components/study/outputs-files-viewer'
 import type { JobFileInfo } from '@/lib/types'
 import type { LatestJobForStudy } from '@/server/db/queries'
 
@@ -28,32 +26,5 @@ export const DecryptAndViewOutputs: FC<DecryptAndViewOutputsProps> = ({ job }) =
         )
     }
 
-    return <DecryptedOutputs jobId={job.id} decryptedFiles={decryptedFiles} />
-}
-
-type DecryptedOutputsProps = {
-    jobId: string
-    decryptedFiles: JobFileInfo[]
-}
-
-const DecryptedOutputs: FC<DecryptedOutputsProps> = ({ jobId, decryptedFiles }) => {
-    const files = useOutputsFiles({ jobId, decryptedFiles })
-    const previewFile = files.viewing ? { name: files.viewing.name, contents: files.viewing.contents } : null
-
-    return (
-        <>
-            <OutputsFilesSection
-                rows={files.rows}
-                isPreparingZip={files.isPreparingZip}
-                onView={files.onView}
-                onDownload={files.onDownload}
-                onDownloadAll={files.onDownloadAll}
-            />
-            <FileOrImagePreviewModal
-                file={previewFile}
-                onClose={files.closeViewer}
-                onDownload={files.onViewerDownload}
-            />
-        </>
-    )
+    return <OutputsFilesViewer jobId={job.id} decryptedFiles={decryptedFiles} />
 }
